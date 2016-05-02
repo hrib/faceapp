f24203eeea2a05059fc22c6481a8b97fc1714698
hugodose <hugo.dose@gmail.com>
2016-05-02 19:37:27 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -8,7 +8,7 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
         document.getElementById('sb_form_q').value = 'oizzsss';
         document.getElementById('scpl1').click();
         
-        return document.title;
+        //return document.title;
         //return  $('href').text();
     });
 
