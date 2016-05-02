5e3cb177a4601b488a78518adbf10ee1da468edd
hugodose <hugo.dose@gmail.com>
2016-05-02 22:22:24 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,23 +1,32 @@
 var page = require('webpage').create();
 
 
-page.open('http://www.bing.com/images/', function() {
+page.open('http://www.bing.com/', function() {
 
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 
-    //var content = page.content;
-    //fs.write("output.html", content, 'w');
-
-  //var busca1 = page.evaluateJavaScript('function(){document.getElementById("sb_form_q").value = "oizzsss"}');
-  //var busca2 = page.evaluateJavaScript('function(){document.getElementById("scpl1").click();}');
-  //console.log(window.); // http://phantomjs.org/img/phantomjs-logo.png
+    page.onResourceReceived = function(response) {
+        if (response.stage !== "end") return;
+        console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + response.url);
+    };
+    page.onResourceRequested = function(requestData, networkRequest) {
+        console.log('Request (#' + requestData.id + '): ' + requestData.url);
+    };
+    page.onUrlChanged = function(targetUrl) {
+        console.log('New URL: ' + targetUrl);
+    };
+    page.onLoadFinished = function(status) {
+        console.log('Load Finished: ' + status);
+    };
+    page.onLoadStarted = function() {
+        console.log('Load Started');
+    };
+    page.onNavigationRequested = function(url, type, willNavigate, main) {
+        console.log('Trying to navigate to: ' + url);
+    };
 
     page.render('bbb.png');
     var resultingHtml = page.evaluate(function() {
-        //document.getElementById("entry_1062721377").value = "oizzsss";
-        //document.getElementById("entry_696307690").value = "2222";
-        //z = document.getElementById("sb_form_q").value;
-        //document.getElementById("u363hi_4").click();
         document.getElementById("sb_form_q").value = "oizzsss"
         var a = document.getElementById("scpl1");
         var e = document.createEvent('MouseEvents');
@@ -25,22 +34,12 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
         a.dispatchEvent(e);
         waitforload = true;
         return document.title;
-            //return document.title;
     });
     page.render('aaa.png');
     console.log(resultingHtml);
 
-  //  search = page.evaluate(function() {
-        //document.getElementById('sb_form_q').value = 'oizzsss';
-        //document.getElementById('scpl1').click();
-      //  return document.title;
-        //return  $('href').text();
-    //});
-
-    //var z = document.title;
-    //console.log(z);
-    //console.log(search);
-    //console.log('chupa');
-    phantom.exit();
+    setTimeout(function(){
+        phantom.exit();
+    }, 10000);
   });
 })
