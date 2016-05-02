f992e21d3f9e12835ba16a368989ba90a32eec4f
hugodose <hugo.dose@gmail.com>
2016-05-02 15:30:45 +0100

    Update get-website.php

--- get-website.php
+++ get-website.php
@@ -1,6 +1,6 @@
 <?php
-    $echo 'oi';
+    echo 'oi';
     $phantom_script= dirname(__FILE__). '/get-website.js'; 
-    $response =  exec ('phantomjs ' . $phantom_script);
+    $response =  exec ('/bin/phantomjs ' . $phantom_script);
     echo  htmlspecialchars($response);
 ?>
