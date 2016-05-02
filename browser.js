3f19132213a685588b28d93d8d6358478559a052
hugodose <hugo.dose@gmail.com>
2016-05-02 18:16:11 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,10 +1,11 @@
 var page = require('webpage').create();
 
-page.open('http://www.bing.com/images', function() {
+page.open('http://www.bing.com/', function() {
 
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 
     search = page.evaluate(function() {
+        window.location = 'http://www.bing.com/images/search?q=zzz';
         return document.title;
         //return  $('href').text();
     });
