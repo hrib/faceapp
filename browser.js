cb9e4a5ee277da536aed9d9a4cc90be209878ec5
hugodose <hugo.dose@gmail.com>
2016-05-02 20:44:45 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -7,17 +7,23 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
     //var content = page.content;
     //fs.write("output.html", content, 'w');
 
-    search = page.evaluate(function() {
+  var busca1 = page.evaluateJavaScript('function(){document.getElementById("sb_form_q").value = "oizzsss"}');
+  var busca2 = page.evaluateJavaScript('function(){document.getElementById("scpl1").click();}');
+  console.log(window); // http://phantomjs.org/img/phantomjs-logo.png
+
+  
+
+  //  search = page.evaluate(function() {
         //document.getElementById('sb_form_q').value = 'oizzsss';
         //document.getElementById('scpl1').click();
-        return document.title;
+      //  return document.title;
         //return  $('href').text();
-    });
+    //});
 
     //var z = document.title;
     //console.log(z);
-    console.log(search);
+    //console.log(search);
     //console.log('chupa');
-    phantom.exit()
+    phantom.exit();
   });
 })
