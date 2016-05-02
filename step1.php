8ad4247cb7c69ebceb2b178f63f4b9e83ba751a1
hugodose <hugo.dose@gmail.com>
2016-05-02 16:20:49 +0100

    Update step1.php

--- step1.php
+++ step1.php
@@ -60,7 +60,7 @@ function pullXmlObjBlogExample($siteUrl,$cssSelector){
 
         echo '<br>aaaaaaaa<br>';
         echo $output;
-        var_dump $output;
+        var_dump($output);
         echo '<br>bbbbb<br>';
         
 
