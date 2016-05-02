b313fdb094713c0e81079a349f8808ca6d1e4831
hugodose <hugo.dose@gmail.com>
2016-05-02 18:38:16 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -5,7 +5,9 @@ page.open('http://www.bing.com/', function() {
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 
     search = page.evaluate(function() {
-        window.location = 'http://www.bing.com/images/search?q=zzz';
+        document.getElementById('sb_form_q').value = 'oizzsss';
+        document.getElementById('scpl1').click();
+        
         return document.title;
         //return  $('href').text();
     });
