4fbb168d956e63c5dee1bd3f84c96bdec37ccdda
hugodose <hugo.dose@gmail.com>
2016-05-02 22:09:15 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 var page = require('webpage').create();
 
 
-page.open('http://www.bing.com/', function() {
+page.open('http://www.bing.com/images/', function() {
 
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 
