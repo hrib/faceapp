518a798fbfc948ac88e41ab1727f70d942efb6ee
hugodose <hugo.dose@gmail.com>
2016-05-02 20:28:01 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,22 +1,23 @@
 var page = require('webpage').create();
 
-page.open('http://www.bing.com/', function() {
+page.open('http://www.bing.com/images', function() {
 
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 
+    //var content = page.content;
+    //fs.write("output.html", content, 'w');
+
     search = page.evaluate(function() {
-        document.getElementById('sb_form_q').value = 'oizzsss';
-        document.getElementById('scpl1').click();
-        
-        //return document.title;
+        //document.getElementById('sb_form_q').value = 'oizzsss';
+        //document.getElementById('scpl1').click();
+        return document.title;
         //return  $('href').text();
     });
 
-    var z = document.title;
-    console.log(z);
-    //console.log(search);
+    //var z = document.title;
+    //console.log(z);
+    console.log(search);
     //console.log('chupa');
-
     phantom.exit()
   });
 })
