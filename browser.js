40c37b22d7976f51ca46baae405e48c9571d008b
hugodose <hugo.dose@gmail.com>
2016-05-02 16:29:16 +0100

    Create browser.js

---
+++ browser.js
@@ -0,0 +1,15 @@
+var page = require('webpage').create();
+
+page.open('http://www.buddytv.com/trivia/game-of-thrones-trivia.aspx', function() {
+
+page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+
+    search = page.evaluate(function() { 
+        return  $('#id60questionText').text();
+    });
+
+    console.log(search);
+
+    phantom.exit()
+  });
+})
