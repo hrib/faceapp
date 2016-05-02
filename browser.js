7eeb16b5fc5cdce9aea9272b1052204395542c43
hugodose <hugo.dose@gmail.com>
2016-05-02 16:38:57 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -4,11 +4,12 @@ page.open('http://www.buddytv.com/trivia/game-of-thrones-trivia.aspx', function(
 
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 
-    search = page.evaluate(function() { 
-        return  $('#id60questionText').text();
-    });
+    //search = page.evaluate(function() { 
+    //    return  $('#id60questionText').text();
+    //});
 
-    console.log('chupa');
+    console.log(page.content);
+    //console.log('chupa');
 
     phantom.exit()
   });
