670fdcb145dd25692410a8a354f177b03b7744cf
hrib <hugo.dose@gmail.com>
2016-05-12 00:42:47 +0100

    Update instagram.php

--- instagram.php
+++ instagram.php
@@ -186,7 +186,7 @@ if(strpos($login[1], "Sorry, an error occurred while processing this request."))
 
                         $media_id = $obj['media_id'];
                         $device_id = "android-".$guid;
-                        $data = '{"share_to_facebook":"1","fb_access_token":"'.$fbtoken.'","device_id":"'.$device_id.'","guid":"'.$guid.'","media_id":"'.$media_id.'","caption":"'.trim($caption).'","device_timestamp":"'.time().'","source_type":"5","filter_type":"0","extra":"{}","Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"}';   
+                        $data = '{"share_to_facebook":"1","fb_access_token":"'.$fbtoken.'","crop_original_size":"'.$w_orig.','.$h_orig.'","crop_zoom":"'.$w_orig/$h_orig.'","crop_center":"0,0","device_id":"'.$device_id.'","guid":"'.$guid.'","media_id":"'.$media_id.'","caption":"'.trim($caption).'","device_timestamp":"'.time().'","source_type":"5","filter_type":"0","extra":"{}","Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"}';   
                         $sig = GenerateSignature($data);
                         $new_data = 'signed_body='.$sig.'.'.urlencode($data).'&ig_sig_key_version=4';
 
