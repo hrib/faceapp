a2286a59fcf4a64272d45f574750132ffbf09997
hrib <hugo.dose@gmail.com>
2016-05-14 17:52:01 +0100

    Update sql.php

--- jogo/sql.php
+++ jogo/sql.php
@@ -13,33 +13,31 @@ $dsn = "pgsql:"
 
 $db = new PDO($dsn);
 
-$query = "DROP TABLE dados";
-//$result = $db->query($query);
-//echo var_dump($result);
-echo 'aqui<br><br>';
+$zquery = "DROP TABLE dados";
 
-$query = "CREATE TABLE dados ("
-    . "id1 VARCHAR(30),"
-    . "id2 VARCHAR(30),"
-    . "id3 VARCHAR(50),"
-    . "id4 VARCHAR(250)"
+$query = "CREATE TABLE Apostas ("
+    . "JogoID VARCHAR(30),"
+    . "PageID VARCHAR(30),"
+    . "PostID VARCHAR(50),"
+    . "CommentID VARCHAR(50),"
+    . "UserID VARCHAR(50),"
+    . "UserName VARCHAR(50),"
+    . "UserAposta VARCHAR(50),"
+    . "UserApostaTime VARCHAR(50)"
     . ");";
-//$result = $db->query($query);
-//echo var_dump($result);
-echo 'aqui<br><br>';
 
-$query = "INSERT INTO dados (id1, id2, id3, id4) VALUES"
+$zquery = "INSERT INTO dados (id1, id2, id3, id4) VALUES"
     . "('xmassage', '121011974285544429', '129b28ee403af9889f18c3fd6f3b9135c8', 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD'),"
     . "('xxx', 'xxxx', 'xxx', 'xx');";
-$query = "UPDATE dados SET id2 = '121011974285544429' , id3 = '129b28ee403af9889f18c3fd6f3b9135c8', id4 = 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD' WHERE id1 = 'xmassage'; ";
-$result = $db->query($query);
-echo var_dump($result);
-echo 'aqui<br><br>';
 
-$query = "SELECT id1, id2, id3, id4 FROM dados";
+$zquery = "UPDATE dados SET id2 = '121011974285544429' , id3 = '129b28ee403af9889f18c3fd6f3b9135c8', id4 = 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD' WHERE id1 = 'xmassage'; ";
+
+$zquery = "SELECT JogoID, PageID, PostID, CommentID, UserID, UserName, UserAposta, UserApostaTime FROM Apostas";
+
+echo $query.'<br>';
 $result = $db->query($query);
 echo var_dump($result);
-echo 'aqui<br><br>';
+echo '<br><br>';
 
 echo "<table>";
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
@@ -52,8 +50,5 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 }
 echo "</table>";
 $result->closeCursor();
-
-//$app->register(new Herrera\Pdo\PdoServiceProvider(), $zica);
-echo 'aqui<br><br>';
 
 ?>
