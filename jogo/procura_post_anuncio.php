02fc6984023afc6307311b0a95926852bf0c1b2b
hrib <hugo.dose@gmail.com>
2016-05-14 16:32:20 +0100

    Update procura_post_anuncio.php

--- jogo/procura_post_anuncio.php
+++ jogo/procura_post_anuncio.php
@@ -62,7 +62,7 @@ foreach($lista_de_pages as $page){
               //foreach($tags_sem_espaco as $tag_sem_espaco)
               //{
                 //echo $tag_sem_espaco.'<br>';
-                //$UserAposta = $tag_sem_espaco;
+              $UserAposta = $tag;
                 //break;
               //}
           }
