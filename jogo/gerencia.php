dc412cb8fb3978849c6b75189b01818743f29399
hrib <hugo.dose@gmail.com>
2016-05-15 01:04:29 +0100

    Update gerencia.php

--- jogo/gerencia.php
+++ jogo/gerencia.php
@@ -1,36 +1,27 @@
 <html>
  <body>
   <h2>Entries</h2>
-  <?php
-  require_once($_SERVER['DOCUMENT_ROOT'].'/consultadb.php');
- 
-  $varum = $_POST['name'];
-  $vardois = $_POST['content'];
-  $sqltodo = $_POST['sql'];
+<?php
   
-  //$sqltodo = "INSERT INTO person (FNAME, LNAME) VALUES ('um','dois')";
-  //$sqltodo = "DELETE from person WHERE ID > 4";
+$query = $_POST['sql'];
   
-  
-  echo $sqltodo;
-  //funciona: $retorno = (new minhaclasse())->usaDB('ALTER TABLE person add column Id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY Id(Id)');
-  //funciona: $retorno = (new minhaclasse())->usaDB("UPDATE person SET FNAME='91560115454240194', LNAME='ze9b7a69cc961d012592996b2dd540e3a' LIMIT 1" );
-  echo '<br>'; 
-  //$retorno = (new minhaclasse())->usaDB("INSERT INTO person (FNAME, LNAME) VALUES ('$varum','$vardois')" );
-  echo '<br>';
-  $retorno = (new minhaclasse())->usaDB("$sqltodo");
-  foreach($retorno as $row) {
-  echo "<div>" . $row[0] . " | " . $row[1] . " | " . $row[2] . " | " . $row[3] . " | " . $row[4] . " | " . $row[5] . " | " . $row[6] . " | " . $row[7] . " | " . $row[8] . " | " . $row[9] . " | " . $row[10] . " | " . $row[11] . " | " . $row[12] . "</div>";
-  }
-  echo '<br>';
- 
-  //$retorno = (new minhaclasse())->usaDB('SELECT * from person');
-  //foreach($retorno as $row) {
-  //echo "<div>" . $row['FNAME'] . " " . $row['LNAME'] . " " . $row['Id'] . "</div>";
-  //}
-  ?>
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+$result = pg_query($pg_conn, $query);
+var_dump($result);
+echo '<br>';
+while ($row = pg_fetch_row($result)) { 
+    foreach($row as $valor)
+    {
+      print("$valor :");
+    }
+    print "\n";
+}
 
-
+?>
 
   <h2>Input</h2>
   <form action="/gerencia.php" method="post">
