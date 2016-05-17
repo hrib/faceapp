99341692dd11ec3c473fc9a7d2047e4294370b71
hrib <hugo.dose@gmail.com>
2016-05-17 20:32:11 +0100

    Update instagram_massage.php

--- instagram_massage.php
+++ instagram_massage.php
@@ -220,7 +220,7 @@ function GetPostData($filename) {
 // Set the username and password of the account that you wish to post a photo to
 $username = 'xmassageuk';
 $password = 'wsimetria1';
-$urlvideo = 'https://video.xx.fbcdn.net/v/t43.1792-2/13121807_1126003104132774_561150169_n.mp4?efg=eyJybHIiOjE1MDAsInJsYSI6MTAyNCwidmVuY29kZV90YWciOiJzdmVfaGQifQ%3D%3D&rl=1500&vabr=218&oh=1a813036c0540d858170eeb7266e4c85&oe=573E4DA3';
+$urlvideo = 'https://video.xx.fbcdn.net/v/t43.1792-2/13185023_1122006934530084_2137460055_n.mp4?efg=eyJybHIiOjE1MDAsInJsYSI6MTAyNCwidmVuY29kZV90YWciOiJzdmVfaGQifQ%3D%3D&rl=1500&vabr=167&oh=d513940abd35bcd3f52cf8235a64be0e&oe=573D2C6B';
 $filename = "video.mp4";
 file_put_contents($filename, file_get_contents($urlvideo));
 //$username = 'london_for_her';
