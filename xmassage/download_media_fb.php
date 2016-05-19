c3035b62deabad341afa0ac032922ed9a8fc1b70
hrib <hugo.dose@gmail.com>
2016-05-19 21:31:10 +0100

    Update download_media_fb.php

--- xmassage/download_media_fb.php
+++ xmassage/download_media_fb.php
@@ -3,12 +3,6 @@ session_start();
 require_once(dirname(__FILE__)."/../src/Facebook/autoload.php");
 
 
-$pageOriginal = '1582615585402238';  //pagina que contem as midias
-$app_id = getenv('FB_APP_ID');
-$app_secret = getenv('FB_APP_SECRET');
-$media = Download_Media_fb($pageOriginal, $app_id, $app_secret);
-echo $media;
-
 function Download_Media_fb($pageOriginal, $app_id, $app_secret){
   $fb = new Facebook\Facebook([
     'app_id' => $app_id,
