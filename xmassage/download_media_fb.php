6a7546e50049340b728ef29e672a78cfd3a1defd
hrib <hugo.dose@gmail.com>
2016-05-19 21:08:33 +0100

    Update download_media_fb.php

--- xmassage/download_media_fb.php
+++ xmassage/download_media_fb.php
@@ -16,19 +16,21 @@ $fb = new Facebook\Facebook([
 $response = $fb->get('/?ids='. $pageOriginal .'&fields=posts.limit(100){source,full_picture,message}');
 $graphNode = $response->getGraphNode();
 
-echo '<table border="1" style="font-family:arial; font-size:7px;">';
+echo '<table border="1" style="font-family:arial; font-size:9px;">';
 foreach ($graphNode as $pagina) {
     $n_posts =  sizeof($pagina['posts']);
     $sorteio_media = mt_rand(0, $n_posts - 1);
     $textos = Array('#massage','#sensual','#book');
     $sorteio_texto = mt_rand(0, sizeof($textos) - 1);
     $media = 'media' . mt_rand(1,999) * mt_rand(1,999);
-    file_put_contents($media.".mp4", file_get_contents($pagina['posts'][$sorteio_media]['source']));
-    file_put_contents($media.".jpg", file_get_contents($pagina['posts'][$sorteio_media]['full_picture']));
     if($pagina['posts'][$sorteio_media]['source']<>""){
       $tipo_media = 'video';
+      $media = $media.'mp4';
+      file_put_contents($media, file_get_contents($pagina['posts'][$sorteio_media]['source']));
     }else{
       $tipo_media = 'foto';
+      $media = $media.'jpg';
+      file_put_contents($media, file_get_contents($pagina['posts'][$sorteio_media]['full_picture']));
     }
       echo '<tr>';
       echo '<td>' . $sorteio_media . ':'. $sorteio_texto .'</td>';
