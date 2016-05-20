ac06eab8fb61d32dce29e8de6d657a60592368e4
hrib <hugo.dose@gmail.com>
2016-05-20 08:37:56 +0100

    Update principal.php

--- xmassage/principal.php
+++ xmassage/principal.php
@@ -44,7 +44,8 @@ echo '<br>'.$media.'<br>';
 
 if($tipo_media == 'foto'){
   echo '<br>JPG<br>';
-  require_once('../Instagram/uploadPhoto.php');
+  //require_once('../Instagram/uploadPhoto.php');
+  require_once('/app/Instagram/uploadPhoto.php');
   Instagram_UploadPhoto($Insta_username, $Insta_passw, $media, $texto);
 }else{
   echo '<br>MP4<br>';
