ea2a80f8ecede49b5276086b5f6cee13ae52190a
hrib <hugo.dose@gmail.com>
2016-05-23 00:23:36 +0100

    Update insta_follow.php

--- xmassage/insta_follow.php
+++ xmassage/insta_follow.php
@@ -4,6 +4,7 @@
     $id_to_get_followers = '327771661';
     
     $url_followedby = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by?access_token='.$token;
+    $url_followedby = preg_replace("/ /", "%20", $url_followedby);
     $result_followedby = file_get_contents($url_followedby);
     var_dump($result_followedby);
     foreach($result_followedby->data as $follower){
