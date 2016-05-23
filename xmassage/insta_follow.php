844fdbe6659324d285a5e488018db8af0b578db6
hrib <hugo.dose@gmail.com>
2016-05-23 11:40:22 +0100

    Update insta_follow.php

--- xmassage/insta_follow.php
+++ xmassage/insta_follow.php
@@ -3,53 +3,64 @@
     $token = getenv('INS_APP_TOKEN');
     $id_to_get_followers = '327771661';
     
-
-
-    
-    
     $url = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by?access_token='.$token;
-    // Initialize session and set URL.
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    
-    // Set so curl_exec returns the result instead of outputting it.
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        
-    // Get the response and close the channel.
     $response = curl_exec($ch);
     curl_close($ch);
     
     $resjson = json_decode($response);
-    var_dump($resjson);    
+    //var_dump($resjson);    
     
+    echo '<table>';
     foreach($resjson->data as $follower){
-        echo $follower->username;
-        echo '<br>';
+        $relacionamento = checaRelacao($follower->id, $token);
+        echo '<tr>';
+        echo '<td>'. $follower->username .'</td>';
+        echo '<td>'. $follower->id .'</td>';
+        echo '<td>'. $relacionamento .'</td>';
+        echo '</tr>';
     }
+    echo '</table>';
  
+    function checaRelacao($userID, $token){
+        $id_to_follow = $userID;
+        $url = 'https://api.instagram.com/v1/users/'.$id_to_follow.'/relationship?access_token='.$token;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        $resjson = json_decode($response);
+        return $resjson->data->outgoing_status;
+    }
     
     
     
-    $id_to_follow = '40911127';
-    $url = 'https://api.instagram.com/v1/users/'.$id_to_follow.'/relationship';
-    $data = array('action' => 'follow', 'access_token' => $token);
-    
-    // use key 'http' even if you send the request to https://...
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    echo $result;
-    echo '<br>';
-
-    $resjson = json_decode($result);
+    function modificaRelacao($userID, $token, $action){
+        $id_to_follow = $userID;
+        $url = 'https://api.instagram.com/v1/users/'.$id_to_follow.'/relationship';
+        $data = array('action' => $action, 'access_token' => $token);
+        
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { /* Handle error */ }
+        echo $result;
+        echo '<br>';
+        $resjson = json_decode($result);
+    }
 
     
 
