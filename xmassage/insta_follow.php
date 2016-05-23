488e288d677068041b25f41a5aa1163a3bde51c0
hrib <hugo.dose@gmail.com>
2016-05-23 09:06:31 +0100

    Update insta_follow.php

--- xmassage/insta_follow.php
+++ xmassage/insta_follow.php
@@ -4,12 +4,36 @@
     $id_to_get_followers = '327771661';
     
     $url_followedby = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by?access_token='.$token;
-    $url_followedby = preg_replace("/ /", "%20", $url_followedby);
+    //$url_followedby = preg_replace("/ /", "%20", $url_followedby);
     $result_followedby = file_get_contents($url_followedby);
     var_dump($result_followedby);
     foreach($result_followedby->data as $follower){
         echo $follower->username;
     }
+    
+    
+    $url_followedby2 = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by';
+    $data = array('access_token' => $token);
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'GET',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url_followedby2, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    
+    var_dump($result);
+    
+    
+    
+    
+    
+    
+    
     
     $curl = curl_init();
     // Set some options - we are passing in a useragent too here
