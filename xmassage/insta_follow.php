1d1bb72b5a7fe18d5860c8d0f09b9e03e66a5ca2
hrib <hugo.dose@gmail.com>
2016-05-23 10:52:00 +0100

    Update insta_follow.php

--- xmassage/insta_follow.php
+++ xmassage/insta_follow.php
@@ -3,50 +3,8 @@
     $token = getenv('INS_APP_TOKEN');
     $id_to_get_followers = '327771661';
     
-    function file_url($url){
-        $parts = parse_url($url);
-        $path_parts = array_map('rawurldecode', explode('/', $parts['path']));
-        return
-            $parts['scheme'] . '://' .
-            $parts['host'] .
-            implode('/', array_map('rawurlencode', $path_parts))
-        ;
-    }
-    
-    
-    
-    
-    
-    $url_followedby = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by?access_token='.$token;
-    $url_followedby = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed%2Dby%3Faccess_token%3D'.$token;
-    //$url_followedby = file_url($url_followedby);
-    echo $url_followedby;
-    //$url_followedby = urlencode($url_followedby);
-    $result_followedby = file_get_contents($url_followedby);
-    var_dump($result_followedby);
-    foreach($result_followedby->data as $follower){
-        echo $follower->username;
-    }
-    echo '<br>';
-    echo '<br>';
-    
-    
-    $url_followedby2 = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by';
-    $data = array('access_token' => $token);
-    // use key 'http' even if you send the request to https://...
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'GET',
-            'content' => http_build_query($data)
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url_followedby2, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    
-    var_dump($result);
-    echo '<br>';
+
+
     
     
     $url = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by?access_token='.$token;
@@ -63,6 +21,9 @@
     var_dump($response);
     curl_close($ch);
     
+    foreach($response->data as $follower){
+        echo $follower->username;
+    }
  
     
     
