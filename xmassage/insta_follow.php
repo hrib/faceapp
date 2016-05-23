ab739ff688cb7c824462f1788850ddfebbc6f192
hrib <hugo.dose@gmail.com>
2016-05-23 10:45:48 +0100

    Update insta_follow.php

--- xmassage/insta_follow.php
+++ xmassage/insta_follow.php
@@ -49,23 +49,21 @@
     echo '<br>';
     
     
+    $url = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/followed-by?access_token='.$token;
+    // Initialize session and set URL.
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
     
+    // Set so curl_exec returns the result instead of outputting it.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        
+    // Get the response and close the channel.
+    $response = curl_exec($ch);
+    var_dump($response);
+    curl_close($ch);
     
-    
-    
-    
-    $curl = curl_init();
-    // Set some options - we are passing in a useragent too here
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $url_followedby,
-        CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-    ));
-    // Send the request & save response to $resp
-    $resp = curl_exec($curl);
-    // Close request to clear up some resources
-    curl_close($curl);
-    //var_dump($resp);
+ 
     
     
     
