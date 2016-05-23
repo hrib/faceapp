8fafc26cf181d91bcd1b795882478ebcfcec71c7
hrib <hugo.dose@gmail.com>
2016-05-23 14:11:26 +0100

    Update insta_unfollow.php

--- xmassage/insta_unfollow.php
+++ xmassage/insta_unfollow.php
@@ -3,39 +3,41 @@
     $token = getenv('INS_APP_TOKEN');
     $id_to_get_followers = 'self';
     
-    $url = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/follows?access_token='.$token.'&count=500'; //.'&count=50'
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    
-    $resjson = json_decode($response);
-    var_dump($resjson);    
-    
-    echo '<table border="1">';
-    $conta = 0;
-    foreach($resjson->data as $follower){
-        //$relacionamento = checaRelacao($follower->id, $token);
-        $acao = '-';
-        if(($relacionamento == 'none') AND ($conta < 10)){
-            //set_time_limit(10); 
-            //sleep(2);
-            //$acao = modificaRelacao($follower->id, $token, 'follow'); 
-            //$acao = 'fakefollow';
-            //$conta = $conta + 1;
-            //sleep(2);
+    for ($x = 0; $x <= 10; $x++) {
+        $url = 'https://api.instagram.com/v1/users/'.$id_to_get_followers.'/follows?access_token='.$token.'&count=100&cursor='.$next_cursor; //.'&count=50'
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        $resjson = json_decode($response);
+        $next_cursor = $resjson->pagination->next_cursor
+        //var_dump($resjson); 
+        echo '<table border="1">';
+        $conta = 0;
+        foreach($resjson->data as $follower){
+            //$relacionamento = checaRelacao($follower->id, $token);
+            $acao = '-';
+            if(($relacionamento == 'none') AND ($conta < 10)){
+                //set_time_limit(10); 
+                //sleep(2);
+                //$acao = modificaRelacao($follower->id, $token, 'follow'); 
+                //$acao = 'fakefollow';
+                //$conta = $conta + 1;
+                //sleep(2);
+            }
+            echo '<tr>';
+            echo '<td>'. $x .'</td>';
+            echo '<td>'. $follower->username .'</td>';
+            echo '<td>'. $follower->id .'</td>';
+            //echo '<td>'. $relacionamento .'</td>';
+            //echo '<td>'. $acao .'</td>';
+            echo '</tr>';
         }
-        echo '<tr>';
-        echo '<td>'. $follower->username .'</td>';
-        echo '<td>'. $follower->id .'</td>';
-        echo '<td>'. $relacionamento .'</td>';
-        echo '<td>'. $acao .'</td>';
-        echo '</tr>';
+        echo '</table>';
     }
-    echo '</table>';
-    
 
     function modificaRelacao($userID, $token, $action){
         echo $userID;
