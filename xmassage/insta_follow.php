d3c8443e8c41fbe52d4f870c1ee308f444a992e1
hrib <hugo.dose@gmail.com>
2016-05-26 10:49:57 +0100

    Update insta_follow.php

--- xmassage/insta_follow.php
+++ xmassage/insta_follow.php
@@ -130,7 +130,7 @@ function FollowSeguidoresdoUsuario($id_to_get_followers, $token){
         
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        //curl_setopt($ch, CURLOPT_HTTPHEADER, $headerData);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headerData);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         //curl_setopt($ch, CURLOPT_HEADER, true);
