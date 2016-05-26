8d5fb6bc1fb601a923ca1cbc44cee3b744da8d21
hrib <hugo.dose@gmail.com>
2016-05-26 12:35:28 +0100

    Create insta_dm_test.php

---
+++ xmassage/insta_dm_test.php
@@ -0,0 +1,33 @@
+<?php
+    require __DIR__.'/../Instagram/src/Instagram.php';
+    CompartilhaMedia('40911127'); 
+
+
+function CompartilhaMedia($userid){    
+    $username = getenv('INSTA_USR_LONDONFORHER');
+    $password = getenv('INSTA_PSW_LONDONFORHER');
+    $debug = true;
+    
+    $media_id = '1258212803463406564_3235184663';
+    $recipients = Array($userid);
+    $text = 'I would like to invite you to check my instagram. 30% discount for new clients. :)';
+
+    $i = new Instagram($username, $password, $debug);
+    
+    try {
+        $i->login();
+    } catch (InstagramException $e) {
+        $e->getMessage();
+        exit();
+    }
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    try {
+        $i->direct_share($media_id, $recipients, $text);
+    } catch (Exception $e) {
+        $e->getMessage();
+    }
+}
+?>
