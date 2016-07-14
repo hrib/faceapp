cee8da8904af033ed426e6231c63198e38ba04b6
hrib <hugo.dose@gmail.com>
2016-07-14 16:42:35 +0100

    Update browser_fb.js

--- browser_fb.js
+++ browser_fb.js
@@ -1,5 +1,4 @@
 var page = require('webpage').create();
-var varin = system.args[1];
 
 page.open('https://m.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token', function() {
 
