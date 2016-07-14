f11935a241e7f048dd3e0355b455e32e464f573b
hrib <hugo.dose@gmail.com>
2016-07-14 17:15:37 +0100

    Update get-facebook.php

--- get-facebook.php
+++ get-facebook.php
@@ -2,8 +2,9 @@
 echo 'oi';
 $pathToPhatomJs = 'bin/phantomjs';
 $pathToJsScript = dirname(__FILE__). '/browser_fb.js';
-$varin = 'qq';
-$stdOut = exec(sprintf('%s %s %s', $pathToPhatomJs,  $pathToJsScript, $varin), $out);
+$varin1 = getenv('email');
+$varin2 = getenv('pass');
+$stdOut = exec(sprintf('%s %s %s %s', $pathToPhatomJs,  $pathToJsScript, $varin1, $varin2), $out);
 
 //$stdOut = exec(sprintf('%s %s', $pathToPhatomJs,  $pathToJsScript), $out);
 echo $stdOut;
