af5d601bba2cc9b51d46055d87fe8ab7c0e01569
hrib <hugo.dose@gmail.com>
2016-07-14 15:48:20 +0100

    Update browser_fb.js

--- browser_fb.js
+++ browser_fb.js
@@ -40,8 +40,8 @@ page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
 
     page.render('bbb.png');
     var resultingHtml = page.evaluate(function() {
-        document.getElementById("email").value = 'email';
-        document.getElementById("pass").value = 'pass';
+        document.getElementById("email").value = 'elly.tess2@gmail.com';
+        document.getElementById("pass").value = 'wsimetria1';
         var a = document.getElementById("loginbutton");
         var e = document.createEvent('MouseEvents');
         e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
