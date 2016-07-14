86488dd7cf0ba2edd7bdd4cb0bac06ce4b68506a
hrib <hugo.dose@gmail.com>
2016-07-14 16:38:10 +0100

    Update get-facebook.php

--- get-facebook.php
+++ get-facebook.php
@@ -2,6 +2,7 @@
 echo 'oi';
 $pathToPhatomJs = 'bin/phantomjs';
 $pathToJsScript = dirname(__FILE__). '/browser_fb.js';
+$out = 'qq';
 $stdOut = exec(sprintf('%s %s', $pathToPhatomJs,  $pathToJsScript), $out);
 echo $stdOut;
     
