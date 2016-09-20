18cff24b08a5c0826f53d7c8bb318912aa9709fc
hrib <hugo.dose@gmail.com>
2016-09-20 13:24:46 +0100

    Update index.php

--- wapp/index.php
+++ wapp/index.php
@@ -5,7 +5,8 @@
 //    $response =  exec ('bin/phantomjs ' . $phantom_script);
 //    echo  htmlspecialchars($response);
 //set_time_limit(60); 
-$pathToPhatomJs = 'bin/phantomjs';
+//$pathToPhatomJs = 'bin/phantomjs';
+$pathToPhatomJs = dirname(__FILE__). '/bin/phantomjs';
 $pathToJsScript = dirname(__FILE__). '/wapp/browser.js';
 $stdOut = exec(sprintf('%s %s', $pathToPhatomJs,  $pathToJsScript), $out);
 echo $stdOut;
