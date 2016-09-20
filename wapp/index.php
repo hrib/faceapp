fca59a37e12616f77dea174ad05836cfc65b98a1
hrib <hugo.dose@gmail.com>
2016-09-20 13:22:58 +0100

    Create index.php

---
+++ wapp/index.php
@@ -0,0 +1,15 @@
+<?php
+    echo 'oi';
+//    $phantom_script= dirname(__FILE__). '/get-website.js'; 
+//    echo $phantom_script;
+//    $response =  exec ('bin/phantomjs ' . $phantom_script);
+//    echo  htmlspecialchars($response);
+//set_time_limit(60); 
+$pathToPhatomJs = 'bin/phantomjs';
+$pathToJsScript = dirname(__FILE__). '/wapp/browser.js';
+$stdOut = exec(sprintf('%s %s', $pathToPhatomJs,  $pathToJsScript), $out);
+echo $stdOut;
+    
+    
+    
+?>
