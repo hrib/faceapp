c04187e3826364b4a4403823f4bac4d113a31863
hrib <hugo.dose@gmail.com>
2016-09-27 14:01:24 +0100

    Update index.php

--- cloneInstagram/index.php
+++ cloneInstagram/index.php
@@ -5,26 +5,51 @@ if($aleatorio < 21){
  echo $aleatorio . " fim";  
  exit;
 }
-require_once(dirname(__FILE__)."/../src/Facebook/autoload.php");
-require_once('download_media_fb.php');
-require_once('post_media_fb.php');
 
 $Insta_username = getenv('INSTA_USR_LONDONFORHER');
 $Insta_passw = getenv('INSTA_PSW_LONDONFORHER');
+$token = getenv('INS_APP_TOKEN');
+$originaluserid = 35380841;
 
-$retorno_media = Download_Media_fb($pageOriginal, $app_id, $app_secret);
-$media = $retorno_media[0];
-$tipo_media = $retorno_media[1];
+$retorno = MediaRecente($originaluserid, $token);
+function MediaRecente($originaluserid, $token){
+    $url = 'https://api.instagram.com/v1/users/'.$originaluserid.'/media/recent/?access_token='.$token;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    
+    $resjson = json_decode($response);
+    var_dump($resjson);    
+    echo '<br>';
+    echo '<table border="1">';
+    $count = 0;
+    $randnum = mt_rand(1,20);
+    foreach($resjson->data as $media){
+        echo '<tr>';
+        echo '<td>'. $media->caption->text .'</td>';
+        echo '<td>'. $media->id .'</td>';
+        echo '</tr>';
+        $count = $count + 1;
+        $ultimoid = $media->id;
+        if($count >= $randnum){break;}
+    }
+    echo '</table>';
+    return $ultimoid;
+}
 
-echo '<br>'.$media.'<br>';
-echo '<br><img src="'.$retorno_media[0].'"><br>';
+
+//echo '<br>'.$media.'<br>';
+//echo '<br><img src="'.$retorno_media[0].'"><br>';
 
 
 if($tipo_media == 'foto'){
   echo '<br>JPG<br>';
   require_once('/app/Instagram/uploadPhoto.php');
   Instagram_UploadPhoto($Insta_username, $Insta_passw, $media, $texto);
-}else{
+//}else{
   echo '<br>MP4<br>';
   $resizemedia = 'resize'.$media;
   shell_exec('/app/vendor/ffmpeg/ffmpeg -i '.$media.' -vf "scale=iw*min(640/iw\,620/ih):ih*min(640/iw\,620/ih),pad=640:620:(640-iw)/2:(620-ih)/2" '.$resizemedia);
