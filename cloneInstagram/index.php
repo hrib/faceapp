eea28341f787956ab2bcc848226fee11ed202c67
hrib <hugo.dose@gmail.com>
2016-09-28 15:01:19 +0100

    Update index.php

--- cloneInstagram/index.php
+++ cloneInstagram/index.php
@@ -70,7 +70,7 @@ function CommentsMediaRecente($mediaID, $token){
     curl_close($ch);
     
     $resjson = json_decode($response);
-    var_dump($resjson);    
+    //var_dump($resjson);    
     echo '<br>';
     echo '<table border="1">';
     foreach($resjson->data as $comment){
