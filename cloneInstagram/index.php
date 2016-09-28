f0fda4331487f3b746471319f087291f08879beb
hrib <hugo.dose@gmail.com>
2016-09-28 14:49:50 +0100

    Update index.php

--- cloneInstagram/index.php
+++ cloneInstagram/index.php
@@ -12,8 +12,55 @@ $token = getenv('INS_APP_TOKEN');
 $originaluserid = 35380841; //3505274959;
 
 $retorno = MediaRecente($originaluserid, $token);
+$mediaID = $retorno[3];
+$ret_comments = CommentsMediaRecente($mediaID, $token);
+
+
 function MediaRecente($originaluserid, $token){
     $url = 'https://api.instagram.com/v1/users/'.$originaluserid.'/media/recent/?access_token='.$token;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    
+    $resjson = json_decode($response);
+    //var_dump($resjson);    
+    echo '<br>';
+    echo '<table border="1">';
+    $count = 0;
+    $randnum = mt_rand(1,20);
+    foreach($resjson->data as $media){
+        echo '<tr>';
+        echo '<td>'. $media->caption->text .'</td>';
+        echo '<td>'. $media->id .'</td>';
+        echo '<td>'. $media->type .'</td>';
+        if($media->type == 'image'){
+          $media_url = $media->images->standard_resolution->url;
+        }else{
+          $media_url = $media->videos->standard_resolution->url;
+        }
+        echo '<td>'. $media_url .'</td>';
+        echo '<td><img src="'.$media_url.'"></td>';
+        echo '</tr>';
+
+        $media_text = $media->caption->text;
+        $media_tipo = $media->type;
+        $media_id = $media->id;
+        
+        $mediadata = [$media_text, $media_tipo, $media_url, $media_id];
+        
+        $count = $count + 1;
+        $ultimoid = $media->id;
+        if($count >= $randnum){break;}
+    }
+    echo '</table>';
+    return $mediadata;
+}
+
+function CommentsMediaRecente($mediaID, $token){
+    $url = 'https://api.instagram.com/v1/media/'.$mediaID.'/comments?access_token='.$token;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
