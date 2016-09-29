e86a328ab021196a6d15629063f0d4ee634d7de0
hrib <hugo.dose@gmail.com>
2016-09-29 17:39:10 +0100

    Update ComentaByTag.php

--- cloneInstagram/ComentaByTag.php
+++ cloneInstagram/ComentaByTag.php
@@ -10,8 +10,30 @@ session_start();
         echo $e->getMessage();
     }
     var_dump($ret_tags);
-
-
+    
+    echo '<br>';
+    echo '<table border="1">';
+    foreach($ret_tags["items"] as $post){
+        echo '<tr>';
+        echo '<td>'. $post["caption"]["text"] .'</td>';
+        echo '<td>'. $post["id"] .'</td>';
+        echo '<td>'. $post["media_type"] .'</td>';
+        if($post["media_type"]  == 1){
+          $post_url = $post["image_versions2"]["candidates"][0]["url"];
+        }else{
+          $post_url = $post["video_versions"][0]["url"];
+        }
+        echo '<td>'. $post_url .'</td>';
+        echo '<td><img src="'.$post_url.'"></td>';
+        echo '</tr>';
+        $post_text = $post["caption"]["text"];
+        $post_tipo = $post["media_type"];
+        $post_id = $post["id"];
+        
+        $postdata = [$post_text, $post_tipo, $post_url, $post_id];
+        //break; //pega so TOP post
+    }
+    echo '</table>';
 
 
 ?>
