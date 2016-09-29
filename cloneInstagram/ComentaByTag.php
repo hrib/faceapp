3a11b00708e7d5ba0a5d47c1370a2199dae1afcc
hrib <hugo.dose@gmail.com>
2016-09-29 17:13:32 +0100

    Update ComentaByTag.php

--- cloneInstagram/ComentaByTag.php
+++ cloneInstagram/ComentaByTag.php
@@ -1,12 +1,14 @@
 <?php
 session_start(); 
 
+    $hashtagString = $matches[0][0];
+    echo '<br><br> hashtag = ' . $hashtagString . '<br>';
     try {
-        $comenta = $i->getHashtagFeed($hashtagString, $maxid = null);
+        $ret_tags = $i->getHashtagFeed($hashtagString, $maxid = null);
     } catch (Exception $e) {
         echo $e->getMessage();
     }
-    var_dump($comenta);
+    var_dump($ret_tags);
 
 
 
