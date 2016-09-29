b4b23da29060818c3e854e6923e0b274eda918a3
hrib <hugo.dose@gmail.com>
2016-09-29 17:20:04 +0100

    Update ComentaByTag.php

--- cloneInstagram/ComentaByTag.php
+++ cloneInstagram/ComentaByTag.php
@@ -2,7 +2,7 @@
 session_start(); 
 
     $hashtagString = str_replace("","#", $matches[0][0]);
-    echo '<br><br> hashtag = ' . $hashtagString . '<br>';
+    echo '<br><br> hashtag = ' . $hashtagString . '<br><br>';
     try {
         $ret_tags = $i->getHashtagFeed($hashtagString, $maxid = null);
     } catch (Exception $e) {
