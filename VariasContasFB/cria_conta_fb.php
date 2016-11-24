8f0a8636cf82a0f3dd6c4eee09d92330e7934080
hrib <hugo.dose@gmail.com>
2016-11-24 16:30:41 +0000

    Update cria_conta_fb.php

--- VariasContasFB/cria_conta_fb.php
+++ VariasContasFB/cria_conta_fb.php
@@ -2,6 +2,13 @@
 session_start();
 session_regenerate_id();
 
+
+
+login();
+
+function login(){
+
+
 $cria_firstname = 'fulano';
 $cria_lastname = 'detal';
 $cria_reg_email__ = 'fulano.de.tal.qqum@gmail.com';
@@ -10,15 +17,11 @@ $cria_birthday_day = '01';
 $cria_birthday_month = '13';
 $cria_birthday_year = '1983';
 $cria_reg_passwd__ = 'senhaqqum';
-
-
-login();
-
-function login(){
-
+    
+    
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/reg/');
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'charset_test=â‚¬,Â´,â‚¬,Â´,æ°´,Ð”,Ð„&firstname=' . urlencode($cria_nome) . '&lastname=' . urlencode($cria_lastname) . '&reg_email__=' . urlencode($cria_reg_email__) . '&sex=' . urlencode($cria_sex) . '&birthday_day=' . urlencode($cria_birthday_day) . '&birthday_month=' . urlencode($cria_birthday_month) . '&birthday_year=' . urlencode($cria_birthday_year) . '&reg_passwd__=' . urlencode($cria_reg_passwd__) . '&submit=' . urlencode('Sign+Up'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'charset_test=â‚¬,Â´,â‚¬,Â´,æ°´,Ð”,Ð„&firstname=' . urlencode($cria_nome) . '&lastname=' . urlencode($cria_lastname) . '&reg_email__=' . urlencode($cria_reg_email__) . '&sex=' . urlencode($cria_sex) . '&birthday_day=' . urlencode($cria_birthday_day) . '&birthday_month=' . urlencode($cria_birthday_month) . '&birthday_year=' . urlencode($cria_birthday_year) . '&reg_passwd__=' . urlencode($cria_reg_passwd__) . '&submit=' . urlencode('Sign+Up') . '&lsd=' . urlencode('AVqytuuQ')  . '&ccp=' . urlencode('5')  . '&mbt=' . urlencode('24')  . '&submission_request=' . urlencode('true')  . '&multi_page_js_state=' . urlencode('passed')  );
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
