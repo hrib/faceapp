9757297a29d548ad0ed4ff2035bc6574782f6177
hrib <hugo.dose@gmail.com>
2016-11-25 14:44:27 +0000

    Update teste.js

--- VariasContasFB/teste.js
+++ VariasContasFB/teste.js
@@ -1,24 +1,12 @@
-"use strict";
+var system = require('system');
 var page = require('webpage').create();
-page.viewportSize = {
-  width: 360,
-  height: 640
-};
-page.settings.javascriptEnabled = true;
-page.settings.loadImages = false;
-phantom.cookiesEnabled = true;
-phantom.javascriptEnabled = true;
-
-// userAgent for Galaxy S5
-//page.settings.userAgent = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2875.0 Mobile Safari/537.36';
-page.open('http://www.globo.com', function (status) {
-    if (status !== 'success') {
-        page.render('fb.png');
-        console.log('Unable to access network');
+page.open('https://www.facebook.com', function(status){
+    console.log("status: " + status);
+    if(status === "success"){
+        console.log("no error");
     } else {
-        setTimeout(funciton () { 
-            page.render('fb.png');
-           phantom.exit();
-        }, 10000);        
-    }  
+        console.log("Error opening url \"" + page.reason_url + "\": " + page.reason);
+    }
+    phantom.exit();
+
 });
