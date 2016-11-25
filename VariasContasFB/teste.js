5adfb5698d153178b338911ce5e4b596ef544c4b
hrib <hugo.dose@gmail.com>
2016-11-25 15:25:18 +0000

    Update teste.js

--- VariasContasFB/teste.js
+++ VariasContasFB/teste.js
@@ -1,13 +1,17 @@
 var system = require('system');
 var page = require('webpage').create();
 page.open('https://www.facebook.com', function(status){
-    console.log("status: " + status);
-    if(status === "success"){
-        console.log("no error");
-    } else {
-        console.log("Error opening url \"" + page.reason_url + "\": " + page.reason);
-    }
-    page.render('fb.png');
-    phantom.exit();
 
+    page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+    
+        console.log("status: " + status);
+        if(status === "success"){
+            console.log("no error");
+        } else {
+            console.log("Error opening url \"" + page.reason_url + "\": " + page.reason);
+        }
+        page.render('fb.png');
+        phantom.exit();
+    });
+    
 });
