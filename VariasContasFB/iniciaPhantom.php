8ad35f07527b231924bb2de8111ed6c049de7800
hrib <hugo.dose@gmail.com>
2016-11-25 14:44:40 +0000

    Update iniciaPhantom.php

--- VariasContasFB/iniciaPhantom.php
+++ VariasContasFB/iniciaPhantom.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Iniciando Phantom. </br>';
 $pathToPhatomJs = dirname(__FILE__). '/../bin/phantomjs';
-$pathToJsScript = dirname(__FILE__). '/browser_SignUpteste.js';
+$pathToJsScript = dirname(__FILE__). '/teste.js';
 
 //$varin1 = getenv('email');
 //$varin2 = getenv('pass');
