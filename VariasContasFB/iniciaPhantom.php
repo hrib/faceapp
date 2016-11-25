b05f47eaa75f67fc3bf4f9a0c717ac0f4130103a
hrib <hugo.dose@gmail.com>
2016-11-25 11:30:34 +0000

    Update iniciaPhantom.php

--- VariasContasFB/iniciaPhantom.php
+++ VariasContasFB/iniciaPhantom.php
@@ -1,13 +1,13 @@
 <?php
-echo 'oi';
+echo 'Iniciando Phantom. </br>';
 $pathToPhatomJs = 'bin/phantomjs';
-$pathToJsScript = dirname(__FILE__). '/browser_fb.js';
-$varin1 = getenv('email');
-$varin2 = getenv('pass');
+$pathToJsScript = dirname(__FILE__). '/browser_SignUp.js';
+//$varin1 = getenv('email');
+//$varin2 = getenv('pass');
+$varin1 = 'meuemail';
+$varin2 = 'meupass';
+
 $stdOut = exec(sprintf('%s %s %s %s', $pathToPhatomJs,  $pathToJsScript, $varin1, $varin2), $out);
-//$stdOut = exec(sprintf('%s %s', $pathToPhatomJs,  $pathToJsScript), $out);
 echo $stdOut;
-    
-    
-    
+echo '</br>Fim Phantom</br>';   
 ?>
