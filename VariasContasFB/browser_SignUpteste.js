cfbcc3c1f6b27c7ef3b796b559ab42aae7ae5256
hrib <hugo.dose@gmail.com>
2016-11-25 13:48:12 +0000

    Update

--- VariasContasFB/browser_SignUpteste.js
+++ VariasContasFB/browser_SignUpteste.js
@@ -2,7 +2,7 @@ var page = require('webpage').create();
 var args = require('system').args;
 
 
-page.open('http://www.globo.com', function() {
+page.open('http://www.facebook.com', function() {
 
 page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
  
