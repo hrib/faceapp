da0668d6c29695f775de385cc4d377370b995a56
hrib <hugo.dose@gmail.com>
2016-11-25 10:37:56 +0000

    Update cria_conta_fb.php

--- VariasContasFB/cria_conta_fb.php
+++ VariasContasFB/cria_conta_fb.php
@@ -17,7 +17,8 @@ $cria_birthday_day = '01';
 $cria_birthday_month = '01';
 $cria_birthday_year = '1983';
 $cria_reg_passwd__ = 'senhaqqum';
-    
+
+echo ('&firstname=' . urlencode($cria_firstname) . '&lastname=' . urlencode($cria_lastname) . '&reg_email__=' . urlencode($cria_reg_email__) . '&sex=' . urlencode($cria_sex) . '&birthday_day=' . urlencode($cria_birthday_day) . '&birthday_month=' . urlencode($cria_birthday_month) . '&birthday_year=' . urlencode($cria_birthday_year) . '&reg_passwd__=' . urlencode($cria_reg_passwd__) . '&submit=' . urlencode('Sign+Up') . '&lsd=' . urlencode('AVqytuuQ')  . '&ccp=' . urlencode('5')  . '&mbt=' . urlencode('24')  . '&submission_request=' . urlencode('true')  . '&multi_page_js_state=' . urlencode('passed'));
     
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/reg/');
@@ -34,7 +35,7 @@ curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd() . '/mirazmac_cookie.txt'); // The 
 curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd() . '/mirazmac_cookie.txt'); // cookie jar
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windo`enter code here`ws; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
-curl_setopt($ch, CURLOPT_REFERER, "https://m.facebook.com/reg/");
+curl_setopt($ch, CURLOPT_REFERER, "http://m.facebook.com");
 $fbMain = curl_exec($ch) or die(curl_error($ch));
 var_dump($fbMain);
 
