1e0cde13c09d03ad540cc6724c39c5bfb2f008ee
hrib <hugo.dose@gmail.com>
2016-11-25 17:22:19 +0000

    Update browser_SignUpteste.js

--- VariasContasFB/browser_SignUpteste.js
+++ VariasContasFB/browser_SignUpteste.js
@@ -13,15 +13,24 @@ page.open('https://www.facebook.com', function(status){
             
            page.onLoadFinished = function(status_fim) {
               texto = texto + '<br>' + 'Loaded'
-              var aprovaApp = page.evaluate(function() {
-                  document.getElementById("u_0_1").value = "teste";
+              //var aprovaApp = page.evaluate(function() {
+                  //document.getElementById("u_0_1").value = "teste";
                   //document.getElementById("u_0_3").value = args[2];
-                  texto = texto + '<br>' + 'Page Eval'
-                  return document.title;
-              });
+                  //texto = texto + '<br>' + 'Page Eval'
+                  //return document.title;
+              //});
 
-           page.render('fb2.png');
+               page.render('fb2.png');
            };
+           
+           setTimeout(function(){
+              page.render('fb3.png');
+              //document.getElementById("u_0_1").value = "teste";
+              texto = texto + '<br>' + 'Pos 5seg'
+           }, 5000);
+           
+           
+           
 
            texto = texto + '<br>' + 'no error'
            console.log(texto);
@@ -29,6 +38,10 @@ page.open('https://www.facebook.com', function(status){
            console.log("Error opening url \"" + page.reason_url + "\": " + page.reason);
         }
         page.render('fb1.png');
+   
+         
+   
+   
          
         setTimeout(function(){
            page.render('fim.png');
