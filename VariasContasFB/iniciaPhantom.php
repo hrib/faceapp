e750aa7b70e6da8cd66fc7f101ce5dede306c70a
hrib <hugo.dose@gmail.com>
2016-11-25 14:13:04 +0000

    Update iniciaPhantom.php

--- VariasContasFB/iniciaPhantom.php
+++ VariasContasFB/iniciaPhantom.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Iniciando Phantom. </br>';
 $pathToPhatomJs = dirname(__FILE__). '/../bin/phantomjs';
-$pathToJsScript = dirname(__FILE__). '/browser_SignUpteste.js';
+$pathToJsScript = dirname(__FILE__). '/teste.js';
 
 //$varin1 = getenv('email');
 //$varin2 = getenv('pass');
@@ -9,7 +9,7 @@ $varin1 = 'meuemail';
 $varin2 = 'meupass';
 // --ssl-protocol=any
 // --ignore-ssl-errors=yes
-$stdOut = exec(sprintf('%s %s %s %s %s', $pathToPhatomJs, '--ssl-protocol=any', $pathToJsScript, $varin1, $varin2), $out);
+$stdOut = exec(sprintf('%s %s %s %s', $pathToPhatomJs, $pathToJsScript, $varin1, $varin2), $out);
 echo $stdOut;
 echo '</br>Fim Phantom</br>';   
 ?>
