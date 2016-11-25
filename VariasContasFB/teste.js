601fd339dc5f49a372ac678379a42378a426c83b
hrib <hugo.dose@gmail.com>
2016-11-25 14:16:36 +0000

    Update teste.js

--- VariasContasFB/teste.js
+++ VariasContasFB/teste.js
@@ -10,9 +10,10 @@ phantom.cookiesEnabled = true;
 phantom.javascriptEnabled = true;
 
 // userAgent for Galaxy S5
-page.settings.userAgent = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2875.0 Mobile Safari/537.36';
-page.open('https://m.facebook.com', function (status) {
+//page.settings.userAgent = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2875.0 Mobile Safari/537.36';
+page.open('http://www.globo.com', function (status) {
     if (status !== 'success') {
+        page.render('fb.png');
         console.log('Unable to access network');
     } else {
         setTimeout(funciton () { 
