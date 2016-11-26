da144bb60279534c6a07903f450b7ee9defd41ba
hrib <hugo.dose@gmail.com>
2016-11-26 15:27:02 +0000

    Update browser_SignUpteste.js

--- VariasContasFB/browser_SignUpteste.js
+++ VariasContasFB/browser_SignUpteste.js
@@ -11,8 +11,8 @@ page.open('https://www.facebook.com', function(status){
         console.log("status: " + status);
         if(status === "success"){
             
-           page.onLoadFinished = function() {
-              page.render('fb2.png');
+           //page.onLoadFinished = function() {
+              //page.render('fb2.png');
               //var aprovaApp = page.evaluate(function() {
                   //document.getElementById("u_0_1").value = "teste";
                   //document.getElementById("u_0_3").value = args[2];
@@ -20,8 +20,8 @@ page.open('https://www.facebook.com', function(status){
                   //return document.title;
               //});
               //return 'Loaded';
-           };
-           texto = texto + '<br>' + sfim;
+           //};
+           //texto = texto + '<br>' + sfim;
                     
            //var title = page.evaluate(function(s) {
            //    page.render('fb3c.png');
