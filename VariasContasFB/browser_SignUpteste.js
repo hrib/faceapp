578c9f21e1d34c43fea3f536b777892cdbd9673c
hrib <hugo.dose@gmail.com>
2016-11-27 18:40:33 +0000

    Update browser_SignUpteste.js

--- VariasContasFB/browser_SignUpteste.js
+++ VariasContasFB/browser_SignUpteste.js
@@ -91,9 +91,9 @@ page.open('https://www.facebook.com', function(status){
            setTimeout(function(){
               page.render('upload.png');
               //page.uploadFile('input[name="file1"]', 'https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e15/11380802_821303507977860_1944379994_n.jpg'); 
-              //page.evaluate(function (args) {
-                 //document.getElementsByName('file1')[0].click();
-              //}, args);   
+              page.evaluate(function (args) {
+                 document.getElementsByName('file1')[0].click();
+              }, args);   
            }, 6000);
            
            
