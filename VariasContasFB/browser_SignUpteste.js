fe3068aeb1bbfc11f462a0eeeb85335bbbcbecc2
hrib <hugo.dose@gmail.com>
2016-11-27 12:52:18 +0000

    Update browser_SignUpteste.js

--- VariasContasFB/browser_SignUpteste.js
+++ VariasContasFB/browser_SignUpteste.js
@@ -6,41 +6,27 @@ page.open('https://www.facebook.com', function(status){
    var email = args[1];
    var pass = args[2];
    var texto = email + pass;
-   //page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
     
         console.log("status: " + status);
         if(status === "success"){
             
-           //page.onLoadFinished = function() {
-              //page.render('fb2.png');
-              //var aprovaApp = page.evaluate(function() {
-                  //document.getElementById("u_0_1").value = "teste";
-                  //document.getElementById("u_0_3").value = args[2];
-                  //texto = texto + '<br>' + 'Page Eval'
-                  //return document.title;
-              //});
-              //return 'Loaded';
-           //};
-           //texto = texto + '<br>' + sfim;
-                    
-           //var title = page.evaluate(function(s) {
-           //    page.render('fb3c.png');
-           //    return 'entrou1'; //document.title;
-           //});
-           //texto = texto + '<br>' + title;
-         
-           //page.onLoadFinished = function(s2) {
-              //texto = texto + '<br>' + 'Loaded';
-              //page.render('Loaded.png');
-           //};
+           page.onLoadFinished = function() {
+              var title2 = page.evaluate(function (s) {
+                 document.getElementsByName("firstname")[0].value = 'loaded'; 
+                 document.getElementsByName("lastname")[0].value = 'loaded'; 
+                 return document.title;
+              }, 'title2');
            
-           var title2 = page.evaluate(function (s) {
+           };
+           page.render('loaded.png');
+           
+           //var title2 = page.evaluate(function (s) {
               //page.render nao funciona aqui e da erro na funcao
-              document.getElementById("u_0_1").value = "1axeval1";
-              document.getElementById("u_0_3").value = "1bxeval1";
-              return document.title;
-           }, 'title2');
-           texto = texto + '<br>' + title2;
+           //   document.getElementById("u_0_1").value = "1axeval1";
+           //   document.getElementById("u_0_3").value = "1bxeval1";
+           //   return document.title;
+           //}, 'title2');
+           //texto = texto + '<br>' + title2;
            
            setTimeout(function(){
               page.render('t3.png');
@@ -72,7 +58,7 @@ page.open('https://www.facebook.com', function(status){
            }, 3000);
            
            setTimeout(function(){
-              page.render('t4.png');
+              page.render('t6.png');
            }, 6000);
            
            
@@ -83,17 +69,11 @@ page.open('https://www.facebook.com', function(status){
         } else {
            console.log("Error opening url \"" + page.reason_url + "\": " + page.reason);
         }
+        
         page.render('fb1.png');
-   
-         
-   
-   
-         
         setTimeout(function(){
            page.render('fim.png');
            phantom.exit();
         }, 10000);
-
-    //});
     
 });
