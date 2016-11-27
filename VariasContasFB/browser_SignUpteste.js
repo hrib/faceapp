ff0320102865743a3176c0ec18d64851c922f64e
hrib <hugo.dose@gmail.com>
2016-11-27 12:53:05 +0000

    Update browser_SignUpteste.js

--- VariasContasFB/browser_SignUpteste.js
+++ VariasContasFB/browser_SignUpteste.js
@@ -49,9 +49,8 @@ page.open('https://www.facebook.com', function(status){
                  
                  document.getElementsByName("sex")[0].checked = false; 
                  document.getElementsByName("sex")[1].checked = true; 
-                 document.getElementsByName('websubmit')[0].click();
-                 //document.getElementById("u_0_i").checked = false;
-                 //document.getElementById("u_0_j").checked = true;
+                 //document.getElementsByName('websubmit')[0].click();
+
               }, args);
               //texto = texto + '<br>' + 'Page Eval';
                   
