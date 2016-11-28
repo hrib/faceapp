5d7b7ab06d0f294f022ee38362c9ae617ffc0a76
hrib <hugo.dose@gmail.com>
2016-11-28 08:25:30 +0000

    Update teste1.js

--- VariasContasFB/teste1.js
+++ VariasContasFB/teste1.js
@@ -17,18 +17,6 @@ page.open('https://www.facebook.com', function(status){
         console.log("status: " + status);
         if(status === "success"){
             
-           //page.onLoadFinished = function() {
-           
-           //};
-           //page.render('loaded.png');
-           
-           //var title2 = page.evaluate(function (s) {
-              //page.render nao funciona aqui e da erro na funcao
-           //   document.getElementById("u_0_1").value = "1axeval1";
-           //   document.getElementById("u_0_3").value = "1bxeval1";
-           //   return document.title;
-           //}, 'title2');
-           //texto = texto + '<br>' + title2;
 
            setTimeout(function(){
               page.render('t2.png');
@@ -46,81 +34,12 @@ page.open('https://www.facebook.com', function(status){
 
            
            
-           
-           
-           setTimeout(function(){
-              page.render('t3.png');
-               
-              page.evaluate(function (args) {
-                 
-                 document.getElementsByName("firstname")[0].value = 'manue'; 
-                 document.getElementsByName("lastname")[0].value = 'loaco'; 
-                 document.getElementsByName("reg_email__")[0].value = 'emailqqnaput1@gmail.com'; 
-                 document.getElementsByName("reg_email_confirmation__")[0].value = 'emailqqnaput1@gmail.com'; 
-                 document.getElementsByName("reg_passwd__")[0].value = 'senhaqq'; 
-                 
-                 document.getElementById("day").value = '10';
-                 document.getElementById("month").value = '11';
-                 document.getElementById("year").value = '1985';
-                 
-                 document.getElementsByName("sex")[0].checked = false; 
-                 document.getElementsByName("sex")[1].checked = true; 
-                 //document.getElementsByName('websubmit')[0].click();
-
-              }, args);
-              //texto = texto + '<br>' + 'Page Eval';
-                  
-           }, 3000);
-           
-          
-           setTimeout(function(){
-              page.render('uploaddesktop1.png'); 
-              //page.evaluate(function (args) {
-                 //var a = document.querySelectorAll('[name*="composer_photo"]');
-                 //var a = document.querySelectorAll('[accept*="video"]'); 
-                 //console.log(a[0]);
-                 //a[0].click();
-              //}, args);   
-           }, 4000);
-           
-            
-           
-           
-           
-           //setTimeout(function(){
-              //page.render('logou.png');
-              //page.open('https://m.facebook.com/photos/upload/?profile_pic&upload_source=profile_pic_upload&profile_pic_source=tagged_photos_page', function(status){
-              //});
-           //}, 4000);
  
-          // Like post on friends wall 
-          //setTimeout(function(){
-          //    page.render('pessoal.png');
-          //    page.evaluate(function (args) {
-          //       var a = document.querySelectorAll('[data-testid="fb-ufi-likelink"]'); 
-          //       a[0].click();
-          //    }, args);   
-          // }, 6000);
           
            setTimeout(function(){
-              page.render('upload.png'); 
-              page.uploadFile('input[name="composer_photo[]"]', 'square.jpg'); 
-              //page.uploadFile('input[name="file1"]', 'square.jpg'); 
-              
-              //page.evaluate(function (args) {
-                // document.getElementsByName('file1')[0].click();
-              //}, args);   
+              page.render('logou.png'); 
+  
            }, 6000);
-           
-           
-           setTimeout(function(){
-              page.render('uploaded.png');
-              page.evaluate(function (args) {
-                 var a = document.querySelectorAll('[data-testid="react-composer-post-button"]'); 
-                 a[0].click();
-              }, args);   
-           }, 10000);
-           
            
           
 
@@ -133,6 +52,6 @@ page.open('https://www.facebook.com', function(status){
         setTimeout(function(){
            page.render('fim.png');
            phantom.exit();
-        }, 15000);
+        }, 10000);
     
 });
