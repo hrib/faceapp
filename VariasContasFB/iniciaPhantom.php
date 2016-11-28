92b9ed243eff09333d0e4f5987b45a1493643083
hrib <hugo.dose@gmail.com>
2016-11-28 12:10:12 +0000

    Update iniciaPhantom.php

--- VariasContasFB/iniciaPhantom.php
+++ VariasContasFB/iniciaPhantom.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Iniciando Phantom. </br>';
 $pathToPhatomJs = dirname(__FILE__). '/../bin/phantomjs';
-$pathToJsScript = dirname(__FILE__). '/browser_SignUpteste.js';
+$pathToJsScript = dirname(__FILE__). '/browser_fbdesktop.js';
 
 $nome = 'Marculano'
 $sobrenome = 'Silvatal'
