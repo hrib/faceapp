e8c88619e99b869116e164ebc9cfc200ebc9c6b8
hrib <hugo.dose@gmail.com>
2016-11-28 08:23:46 +0000

    Update teste.php

--- VariasContasFB/teste.php
+++ VariasContasFB/teste.php
@@ -1,15 +1,20 @@
 <?php
 echo 'Iniciando Phantom. </br>';
 $pathToPhatomJs = dirname(__FILE__). '/../bin/phantomjs';
-$pathToJsScript = dirname(__FILE__). '/browser_SignUpteste.js';
+
 $varin1 = getenv('email');
 $varin2 = getenv('pass');
-//$varin1 = 'meuemail';
-//$varin2 = 'meupass';
-// --ssl-protocol=any
-// --ignore-ssl-errors=yes
+
 $stdOutv = exec(sprintf('%s %s', $pathToPhatomJs, '--version'), $out);
 echo $stdOutv . "<br/>";
+
+$pathToJsScript = dirname(__FILE__). '/teste1.js';
+$stdOut = exec(sprintf('%s %s %s %s %s', $pathToPhatomJs, '--ssl-protocol=any --ignore-ssl-errors=yes', $pathToJsScript, $varin1, $varin2), $out);
+echo $stdOut;
+
+echo '</br></br>'; 
+
+$pathToJsScript = dirname(__FILE__). '/teste2.js';
 $stdOut = exec(sprintf('%s %s %s %s %s', $pathToPhatomJs, '--ssl-protocol=any --ignore-ssl-errors=yes', $pathToJsScript, $varin1, $varin2), $out);
 echo $stdOut;
 echo '</br>Fim Phantom</br>';   
