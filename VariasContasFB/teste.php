d09019e01a8980d98b9f51e0a44ddb81c6bf9d2f
hrib <hugo.dose@gmail.com>
2016-11-28 08:39:29 +0000

    Update teste.php

--- VariasContasFB/teste.php
+++ VariasContasFB/teste.php
@@ -11,5 +11,9 @@ echo $stdOutv . "<br/>";
 $pathToJsScript = dirname(__FILE__). '/teste1.js';
 $stdOut = exec(sprintf('%s %s %s %s %s', $pathToPhatomJs, '--ssl-protocol=any --ignore-ssl-errors=yes', $pathToJsScript, $varin1, $varin2), $out);
 echo $stdOut;
-echo '</br>Fim Phantom</br>';   
+echo '</br>Fim Phantom</br>';  
+echo '<img src="t2.png" >';
+echo '<img src="logou.png" >';
+echo '<img src="finished.png" >';
+echo '<img src="fim.png" >';
 ?>
