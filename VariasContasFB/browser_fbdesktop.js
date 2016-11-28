e100d3ddd36c81f78df990d683c5b89bf39bb6f7
hrib <hugo.dose@gmail.com>
2016-11-28 19:29:35 +0000

    Update browser_fbdesktop.js

--- VariasContasFB/browser_fbdesktop.js
+++ VariasContasFB/browser_fbdesktop.js
@@ -77,9 +77,9 @@ page.open('https://www.facebook.com', function(status){
           //    }, args);   
           // }, 6000);
         
-        var tarefas = [myprofile(), click_input()]; 
+        var tarefas = [myprofile, click_input]; 
         //navigate to profile page        
-        //setTimeout(tarefas[0], 5000);    
+        setTimeout(tarefas[0](), 5000);    
         //post profile pic
 //        setTimeout(function(){
 //            page.evaluate(function (args) {
