6a48428004e332e6b2d02dffa1bf30729aff0ea7
hrib <hugo.dose@gmail.com>
2016-11-30 09:47:21 +0000

    Update iniciaPhantom.php

--- VariasContasFB/iniciaPhantom.php
+++ VariasContasFB/iniciaPhantom.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 echo 'Iniciando Phantom. </br>';
 $pathToPhatomJs = dirname(__FILE__). '/../bin/phantomjs';
 $pathToJsScript = dirname(__FILE__). '/browser_fbdesktop.js';
@@ -13,9 +14,14 @@ $year = '1985';
 $sex = 'male'; // male|female
 $signtype = 'signin'; // signup|signin
 $anotherURL = 'https://www.facebook.com/profile.php?id=100009466980633'; // URL da wall para dar like no post
+?>
 
+<form action="sql.php" method="post">
+SQL <input type="text" name="query"><br>
+<input type="submit">
+</form>
 
-CreateTable();
+<?php
 
 $stdOutv = exec(sprintf('%s %s', $pathToPhatomJs, '--version'), $out);
 echo $stdOutv . "<br/>";
@@ -37,32 +43,6 @@ echo '<br>like_post_on_another_wall</br><img src="like_post_on_another_wall.png"
 
 echo '<br>fim</br><img src="fim.png" style="width:250px;height:250px;">';
 
-function CreateTable(){
-  $dbopts = parse_url(getenv('DATABASE_URL'));
-  $dsn = "pgsql:"
-      . "host=" . $dbopts["host"] . ";"
-      . "dbname=". ltrim($dbopts["path"],'/') . ";"
-      . "user=" . $dbopts["user"] . ";"
-      . "port=" . $dbopts["port"] . ";"
-      . "sslmode=require;"
-      . "password=" . $dbopts["pass"];
 
-  $db = new PDO($dsn);
-
-  $query = "CREATE TABLE Users ("
-      . "email VARCHAR(30),"
-      . "passw VARCHAR(10),"
-      . "nome VARCHAR(20),"
-      . "sobrenome VARCHAR(20),"
-      . "day VARCHAR(2),"
-      . "month VARCHAR(2),"
-      . "year VARCHAR(4),"
-      . "sex VARCHAR(10),"
-      . "ultimo_acesso VARCHAR(30),"
-      . "status VARCHAR(30)"
-      . ");";
-  $result = $db->query($query);
-  echo var_dump($result);
-}
 
 ?>
