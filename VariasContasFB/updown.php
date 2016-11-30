6b18ffc5abde3e3b59e336a673a2c7304b8f462d
hrib <hugo.dose@gmail.com>
2016-11-30 16:03:10 +0000

    Update updown.php

--- VariasContasFB/updown.php
+++ VariasContasFB/updown.php
@@ -18,10 +18,10 @@ $data = file_get_contents('http://xoax.net/cpp/ref/cstd/incl/cstdio/fn/fwrite/fw
 $escaped = bin2hex( $data );
 
 // Insert it into the database
-pg_query( "INSERT INTO upload (name, content) VALUES ('Pine trees', decode('{$escaped}' , 'hex'))" );
+pg_query($dbconn, "INSERT INTO upload (name, content) VALUES ('Pine trees', decode('{$escaped}' , 'hex'))" );
 
 // Get the bytea data
-$res = pg_query("SELECT encode(data, 'base64') AS data FROM upload WHERE name='Pine trees'");  
+$res = pg_query($dbconn, "SELECT encode(data, 'base64') AS data FROM upload WHERE name='Pine trees'");  
 $raw = pg_fetch_result($res, 'data');
 
 // Convert to binary and send to the browser
