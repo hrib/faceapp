704f7b211d9a099ac9b995f569104e23b422c58f
hrib <hugo.dose@gmail.com>
2016-11-30 11:22:34 +0000

    Update sql.php

--- VariasContasFB/sql.php
+++ VariasContasFB/sql.php
@@ -23,6 +23,24 @@ function SQLquery($query){
   echo var_dump($result);
   echo '<br><br>';
   
+  
+  echo "<table>";
+  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+  echo "<tr>";
+  echo "<td>" . $row["email"] . "</td>";
+  echo "<td>" . $row["passw"] . "</td>";
+  echo "<td>" . $row["nome"] . "</td>";
+  echo "<td>" . $row["sobrenome"] . "</td>";
+  echo "<td>" . $row["day"] . "</td>";
+  echo "<td>" . $row["month"] . "</td>";
+  echo "<td>" . $row["year"] . "</td>";
+  echo "<td>" . $row["sex"] . "</td>";
+  echo "<td>" . $row["ultimo_acesso"] . "</td>";
+  echo "<td>" . $row["status"] . "</td>";
+  echo "</tr>";
+  echo "</table>";
+  $result->closeCursor();
+  
 }
 
 
