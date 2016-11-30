fa8aa26bf033db4b1eb407dd5e108d29e4e22921
hrib <hugo.dose@gmail.com>
2016-11-30 14:50:27 +0000

    Update download.php

--- VariasContasFB/download.php
+++ VariasContasFB/download.php
@@ -39,10 +39,14 @@ if (!$conn) {
   
   
 pg_query('SET bytea_output = "escape";');
-$lquery ="select content from upload";
+$lquery ="select size, type, content from upload";
 $lq = pg_query($conn, $lquery) or die(pg_last_error());
 $lqq=pg_fetch_row($lq,'content');
-header("conent-type:image");
+$sss=pg_fetch_row($lq,'size');
+$ttt=pg_fetch_row($lq,'type');
+  
+header("Content-length:" . $sss[0]);
+header("Content-type:" . $ttt[0]);
 echo pg_unescape_bytea($lqq[0]);  
   
   
