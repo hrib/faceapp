3d91421f13493417bead6da3ceb785e7273761b1
hrib <hugo.dose@gmail.com>
2016-12-01 10:36:40 +0000

    Update sql.php

--- VariasContasFB/sql.php
+++ VariasContasFB/sql.php
@@ -21,15 +21,15 @@ function SQLquery($query){
   $result = $db->query($query);
   
   echo '<br><br>';
-  echo var_dump($result);
+  //echo var_dump($result);
   echo '<br><br>';
   
   
   echo "<table>";
   while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     echo "<tr>";
-    echo "<td>" . $row["email"] . "</td>";
-    echo "<td>" . $row["passw"] . "</td>";
+    echo "<td>" . $row["tipo_nome"] . "</td>";
+    echo "<td>" . $row["nome"] . "</td>";
     echo "<td>" . $row["nome"] . "</td>";
     echo "<td>" . $row["sobrenome"] . "</td>";
     echo "<td>" . $row["day"] . "</td>";
@@ -39,13 +39,13 @@ function SQLquery($query){
     echo "<td>" . $row["ultimo_acesso"] . "</td>";
     echo "<td>" . $row["status"] . "</td>";
     echo "<td>" . $row["name"] . "</td>";
-    echo "<td>content: " . $row["content"] . "</td>";
-     echo "<td>content: " . pg_unescape_bytea($row["content"]) . "</td>";
-     echo "<td>content: " . pg_escape_bytea($row["content"]) . "</td>";
-        $raw = base64_encode($row["content"]);
-    echo "<td>encode: " . $raw . "</td>";
-        $dataUri = "data:image/jpeg;base64," . $raw;
-        echo "<td>image: <img src='$dataUri' /></td>";
+    //echo "<td>content: " . $row["content"] . "</td>";
+     //echo "<td>content: " . pg_unescape_bytea($row["content"]) . "</td>";
+     //echo "<td>content: " . pg_escape_bytea($row["content"]) . "</td>";
+        //$raw = base64_encode($row["content"]);
+    //echo "<td>encode: " . $raw . "</td>";
+        //$dataUri = "data:image/jpeg;base64," . $raw;
+        //echo "<td>image: <img src='$dataUri' /></td>";
         //$dataUri = "data:image/jpeg;base64," . $row["data"];
         //echo "<td><img src='$dataUri' /></td>";
     
