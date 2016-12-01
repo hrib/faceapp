847ff19e791d77f6232db709ffd216b66536829a
hrib <hugo.dose@gmail.com>
2016-12-01 11:15:28 +0000

    Update GeraNome.php

--- VariasContasFB/GeraNome.php
+++ VariasContasFB/GeraNome.php
@@ -10,10 +10,12 @@ function GeraNome(){
   
   $query = "SELECT nome FROM bercario WHERE tipo_nome = '$sex'";
   $retorno_firstname = SQLquery($query);
-  $nome['firstname'] = $retorno_firstname[mt_rand(0, count($retorno_firstname) - 1)];
+  //$nome['firstname'] = $retorno_firstname[mt_rand(0, count($retorno_firstname) - 1)];
+  $nome['firstname'] = $retorno_firstname[1];
+  echo $nome['firstname'];
   $query = "SELECT nome FROM bercario WHERE tipo_nome = 'sobrenome'";
   $retorno_lastname = SQLquery($query);
-  $nome['lastname'] = $retorno_lastname[mt_rand(0, count($retorno_lastname) - 1)];
+  //$nome['lastname'] = $retorno_lastname[mt_rand(0, count($retorno_lastname) - 1)];
   return $nome; 
 }
 
