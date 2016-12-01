fffb633e739c0bf670916c13d94d50f4b7fa0f95
hrib <hugo.dose@gmail.com>
2016-12-01 14:32:25 +0000

    Update iniciaPhantom.php

--- VariasContasFB/iniciaPhantom.php
+++ VariasContasFB/iniciaPhantom.php
@@ -55,9 +55,13 @@ echo '<br>fim</br><img src="fim.png" style="width:250px;height:250px;">';
 
 function strip_punctuation($string) {
     $string = strtolower($string);
+    echo $string . '|';
     $string = preg_replace('/[^a-z]+/i', '_', $string); 
+    echo $string . '|';
     //$string = preg_replace("/[^a-zA-Z 0-9]+/", "", $string);
     //$string = str_replace(" ", ".", $string);
+    $string  = rtrim($string);
+    echo $string . '|';
     return $string;
 }
 
