02f785318ca89dbd5e0ac0011b3fdd1a1bb74918
hrib <hugo.dose@gmail.com>
2017-05-09 23:14:46 +0100

    Update save_user_page.php

--- trocalikes/save_user_page.php
+++ trocalikes/save_user_page.php
@@ -14,8 +14,8 @@ $db = new PDO($dsn);
 
 $user_name = $_SESSION["user_name"];
 $user_id = $_SESSION["user_id"];
-
-$query = "UPDATE tl_cadastro SET pagina = " . $_POST['new_user_page'] . " WHERE user_id = '" . $user_id . "';";
+echo $_POST['new_user_page'] ;
+$query = "UPDATE tl_cadastro SET pagina = '" . $_POST['new_user_page'] . "' WHERE user_id = '" . $user_id . "';";
 $result = $db->query($query);
 echo "<script>window.top.location.href='https://apps.facebook.com/trocalikes'</script>";   
     
