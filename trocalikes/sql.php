d12da0905f11cb94b9062af7487e3cf06d0785b5
hrib <hugo.dose@gmail.com>
2017-05-09 22:05:05 +0100

    Update sql.php

--- trocalikes/sql.php
+++ trocalikes/sql.php
@@ -47,18 +47,18 @@ $user_name = "seis";
 $query = "INSERT INTO tl_cadastro(user_id, user_name) SELECT '" . $user_id . "', '" . $user_name . "' FROM tl_cadastro where not exists (select 1 from tl_cadastro where user_id = '" . $user_id . "');";
 $result = $db->query($query);
 
-    $query = "SELECT user_name FROM tl_cadastro WHERE user_id = '" . $user_id . "';";
-    $result = $db->query($query);
-    $row = $result->fetch();
-    echo $row["user_name"];
+//    $query = "SELECT user_name FROM tl_cadastro WHERE user_id = '" . $user_id . "';";
+//    $result = $db->query($query);
+//    $row = $result->fetch();
+//    echo $row["user_name"];
 
 
 
 
 $query = "SELECT id, user_id, user_name, pagina FROM tl_cadastro;";
 $result = $db->query($query);
-echo var_dump($result);
-echo 'aqui resultados<br><br>';
+//echo var_dump($result);
+//echo 'aqui resultados<br><br>';
 
 echo '<table border="1" style="font-family:arial; font-size:7px;">';
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
