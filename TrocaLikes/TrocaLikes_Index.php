f13d481bad7115acbb3bd9e1cb3bc27ff39cc2e7
hrib <hugo.dose@gmail.com>
2017-05-09 12:07:34 +0100

    Update TrocaLikes_Index.php

--- TrocaLikes/TrocaLikes_Index.php
+++ TrocaLikes/TrocaLikes_Index.php
@@ -13,6 +13,37 @@ $fb = new Facebook\Facebook([
   'default_access_token' => $app_id . '|' . $app_secret
 ]);
 
+
+
+$helper = $fb->getCanvasHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+if (! isset($accessToken)) {
+  echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
+  exit;
+}
+
+// Logged in
+echo '<h3>Signed Request</h3>';
+var_dump($helper->getSignedRequest());
+
+echo '<h3>Access Token</h3>';
+var_dump($accessToken->getValue());
+
+
+
+
 try {  
   $response = $fb->get('/me');
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
