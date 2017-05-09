0e9b370238adcee60e3bbc96ec073f28ee2506c3
hrib <hugo.dose@gmail.com>
2017-05-09 19:30:33 +0100

    Update sql.php

--- trocalikes/sql.php
+++ trocalikes/sql.php
@@ -17,12 +17,12 @@ $query = "CREATE TABLE tl_cadastro ("
     . "ID int, "
     . "user_id int, "
     . "user_name VARCHAR(50), "
-    . "pagina VARCHAR(80) "
+    . "pagina VARCHAR(50) "
     . ");";
 $result = $db->query($query);
-echo var_dump($result);
+//echo var_dump($result);
 echo 'aqui<br><br>';
-$query = "INSERT INTO tl_cadastro(user_id, user_name, pagina) VALUES('19', 'fulano de tal muito louco', 'http://facebook.com/rconstantinoliberal/');";
+$query = "INSERT INTO tl_cadastro(user_id, user_name, pagina) VALUES(19, 'fulano de tal', 'http://facebook.com/rconstantinoliberal/');";
 
 //$query = "UPDATE dados SET id2 = '121011974285544429' , id3 = '129b28ee403af9889f18c3fd6f3b9135c8', id4 = 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD' WHERE id1 = 'xmassage'; ";
 $result = $db->query($query);
