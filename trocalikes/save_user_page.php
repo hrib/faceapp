80741dbf47d3cd1ca7c98becf1272542c2b62eeb
hrib <hugo.dose@gmail.com>
2017-05-09 22:48:44 +0100

    Create save_user_page.php

---
+++ trocalikes/save_user_page.php
@@ -0,0 +1,24 @@
+<?php
+function db_usuario($user_id, $user_name){
+    
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$dsn = "pgsql:"
+    . "host=" . $dbopts["host"] . ";"
+    . "dbname=". ltrim($dbopts["path"],'/') . ";"
+    . "user=" . $dbopts["user"] . ";"
+    . "port=" . $dbopts["port"] . ";"
+    . "sslmode=require;"
+    . "password=" . $dbopts["pass"];
+    
+$db = new PDO($dsn);
+    
+    $query = "INSERT INTO tl_cadastro(user_id, user_name) SELECT '" . $user_id . "', '" . $user_name . "' where not exists (select id from tl_cadastro where user_id = '" . $user_id . "') RETURNING id;";
+    $result = $db->query($query);
+    
+    $query = "SELECT pagina FROM tl_cadastro WHERE user_id = '" . $user_id . "';";
+    $result = $db->query($query);
+    $retorno = $result->fetch();
+    return $retorno["pagina"];
+    
+}
+?>
