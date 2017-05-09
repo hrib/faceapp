af9e61c071bc989662efae2e49174d9b160f7604
hrib <hugo.dose@gmail.com>
2017-05-09 19:08:18 +0100

    Update sql.php

--- trocalikes/sql.php
+++ trocalikes/sql.php
@@ -9,37 +9,37 @@ $dsn = "pgsql:"
     . "password=" . $dbopts["pass"];
     
 $db = new PDO($dsn);
-$query = "DROP TABLE dados";
+//$query = "DROP TABLE dados";
 //$result = $db->query($query);
 //echo var_dump($result);
 echo 'aqui<br><br>';
-$query = "CREATE TABLE dados ("
-    . "id1 VARCHAR(30),"
-    . "id2 VARCHAR(30),"
-    . "id3 VARCHAR(50),"
-    . "id4 VARCHAR(250)"
+$query = "CREATE TABLE tl_cadastro ("
+    . "ID int NOT NULL AUTO_INCREMENT, "
+    . "user_id int, "
+    . "user_name VARCHAR(50), "
+    . "pagina VARCHAR(80), "
+    . " PRIMARY KEY (ID) "
     . ");";
 //$result = $db->query($query);
 //echo var_dump($result);
 echo 'aqui<br><br>';
-$query = "INSERT INTO dados (id1, id2, id3, id4) VALUES"
-    . "('xmassage', '121011974285544429', '129b28ee403af9889f18c3fd6f3b9135c8', 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD'),"
-    . "('xxx', 'xxxx', 'xxx', 'xx');";
-$query = "UPDATE dados SET id2 = '121011974285544429' , id3 = '129b28ee403af9889f18c3fd6f3b9135c8', id4 = 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD' WHERE id1 = 'xmassage'; ";
+$query = "INSERT INTO tl_cadastro(user_id, user_name, pagina) VALUES('121011974285544429', 'fulano de tal muito louco', 'http://facebook.com/rconstantinoliberal/')";
+
+//$query = "UPDATE dados SET id2 = '121011974285544429' , id3 = '129b28ee403af9889f18c3fd6f3b9135c8', id4 = 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD' WHERE id1 = 'xmassage'; ";
 $result = $db->query($query);
 echo var_dump($result);
 echo 'aqui<br><br>';
-$query = "SELECT id1, id2, id3, id4 FROM dados";
+$query = "SELECT ID, user_id, user_name, pagina FROM tl_cadastro";
 $result = $db->query($query);
 echo var_dump($result);
 echo 'aqui<br><br>';
 echo "<table>";
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     echo "<tr>";
-    echo "<td>" . $row["id1"] . "</td>";
-    echo "<td>" . htmlspecialchars($row["id2"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["id3"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["id4"]) . "</td>";
+    echo "<td>" . $row["ID"] . "</td>";
+    echo "<td>" . htmlspecialchars($row["user_id"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["user_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["pagina"]) . "</td>";
     echo "</tr>";
 }
 echo "</table>";
