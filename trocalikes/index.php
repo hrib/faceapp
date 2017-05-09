a18fd4bd59d0f099cdc86efe16cab58d3082e4b4
hrib <hugo.dose@gmail.com>
2017-05-09 22:38:21 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -67,12 +67,13 @@ try {
  exit;
 }
 
-$user_id = '12345678901234567890';
 $graphNode = $response->getGraphNode();
 $user_name = $graphNode['name'];
 echo $user_name . '<br>';
 $user_id = $graphNode['id'];
 echo $user_id . '<br>';
+
+$user_id = '12345678901234567890';
 $user_page = db_usuario($user_id, $user_name);
 echo $user_page . '<br>';
 
