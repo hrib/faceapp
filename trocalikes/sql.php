3017d29e0228c7d54cfd2fa4b2d62664620db6db
hrib <hugo.dose@gmail.com>
2017-05-09 21:28:50 +0100

    Update sql.php

--- trocalikes/sql.php
+++ trocalikes/sql.php
@@ -24,7 +24,7 @@ $query = "CREATE TABLE tl_cadastro ("
 $result = $db->query($query);
 //echo var_dump($result);
 echo 'aqui<br><br>';
-$query = "INSERT INTO tl_cadastro(zz, zzz, user_id, user_name, pagina) VALUES(2234567890098765432, 3, 4, 'fulano de tal', 'http://facebook.com/rconstantinoliberal/');";
+$query = "INSERT INTO tl_cadastro(zz, zzz, user_id, user_name, pagina) VALUES(65432, 3, 4, 'fulano de tal', 'http://facebook.com/rconstantinoliberal/');";
 
 //$query = "UPDATE dados SET id2 = '121011974285544429' , id3 = '129b28ee403af9889f18c3fd6f3b9135c8', id4 = 'E12AAOYYpZCPyZB0BALd0WuUAuWTWKHIUCGzvCiB8jY3RwLZAUpdpvb7d7tmhIbmNcZAuIxX1vYsZAQQkSuHQ3TknkLDGHLQcnJ2oyVJZCtaRXPqCmblfcNjy3S5ZCgw574urWAggppaIKCP6rpQvD0ObUKh8pnnH7KOzo2352mZCHuzgZDZD' WHERE id1 = 'xmassage'; ";
 $result = $db->query($query);
