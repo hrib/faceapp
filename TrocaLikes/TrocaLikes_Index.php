20f7dec21326105a4b2ffecf25a4b26bce78d11f
hrib <hugo.dose@gmail.com>
2017-05-09 10:37:11 +0100

    Update TrocaLikes_Index.php

--- TrocaLikes/TrocaLikes_Index.php
+++ TrocaLikes/TrocaLikes_Index.php
@@ -1,10 +1,29 @@
 <?php
 session_start(); 
 require_once(dirname(__FILE__)."/../src/Facebook/autoload.php");
+?>
+<style>
+html { 
+  background: url("http://www.planwallpaper.com/static/images/Alien_Ink_2560X1600_Abstract_Background_1.jpg") no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+<div class="dentro">
+  <form action="save_userpage_url.php" method="post">
+      <table  border="0">
+        <tr valign="middle">
+          <td><font style="font-family:arial; font-size:10px;"><b>Digite a URL da sua página: </b></font></td>
+          <td align="left"><input type="text" name="user" style="font-family:arial; font-size:10px; width: 180px; margin-left: 0px; margin-top: 0px;"></td>
+        </tr>
+      </table>
+      <input type="image" style="width:280px;height:105px;">
+  </form>
+</div>
 
-echo 'Digite a URL da sua pagina: <input type="text" name="userpage_url">';
-
-
+<?php
 $app_id = getenv('FB_APP_ID');
 $app_secret = getenv('FB_APP_SECRET');
 $paginaID = 'rconstantinoliberal';
