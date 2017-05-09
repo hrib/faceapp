77f96235b827149fd1148f2b932080c297eadfac
hrib <hugo.dose@gmail.com>
2017-05-09 21:38:44 +0100

    Update sql.php

--- trocalikes/sql.php
+++ trocalikes/sql.php
@@ -45,9 +45,10 @@ $result = $db->query($query);
 
     $query = "SELECT pagina FROM tl_cadastro WHERE user_id = " . $user_id . ";";
     $result = $db->query($query);
-    //echo $result["user_name"];
-    $row = $result->fetch(PDO::FETCH_ASSOC);
+    $row = $result->fetch();
     echo $row["user_name"];
+    echo $row[0];
+    echo $row[1];
 
 
 
