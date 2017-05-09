c8c3d2dbc3b370f2ae4efb84fb625d3bcef2fb5f
hrib <hugo.dose@gmail.com>
2017-05-09 22:00:53 +0100

    Update sql.php

--- trocalikes/sql.php
+++ trocalikes/sql.php
@@ -41,6 +41,12 @@ $user_name = "cinco";
 $query = "INSERT INTO tl_cadastro(user_id, user_name) SELECT '" . $user_id . "', '" . $user_name . "' FROM tl_cadastro where not exists (select 1 from tl_cadastro where user_id = '" . $user_id . "');";
 $result = $db->query($query);
 
+$user_id = '6';
+$user_name = "seis";
+
+$query = "INSERT INTO tl_cadastro(user_id, user_name) SELECT '" . $user_id . "', '" . $user_name . "' FROM tl_cadastro where not exists (select 1 from tl_cadastro where user_id = '" . $user_id . "');";
+$result = $db->query($query);
+
     $query = "SELECT user_name FROM tl_cadastro WHERE user_id = '" . $user_id . "';";
     $result = $db->query($query);
     $row = $result->fetch();
