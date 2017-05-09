5ea9f0d2a5b3f1fff8268041a009552fc7023e21
hrib <hugo.dose@gmail.com>
2017-05-09 13:15:39 +0100

    Update Index.php

--- TrocaLikes/Index.php
+++ TrocaLikes/Index.php
@@ -22,11 +22,11 @@ try {
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
   echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
+  //exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
   // When validation fails or other local issues
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
+  //exit;
 }
 
 if (! isset($accessToken)) {
