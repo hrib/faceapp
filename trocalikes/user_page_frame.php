aa2e84a9b70747d020aac73944934414feebe633
hrib <hugo.dose@gmail.com>
2017-05-10 10:33:50 +0100

    Create user_page_frame.php

---
+++ trocalikes/user_page_frame.php
@@ -0,0 +1,15 @@
+<div class="dentro">
+  <form action="save_user_page.php" method="post">
+      <table  border="0">
+        <tr valign="middle">
+          <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:16px;"><b>Digite a URL da sua página: </b></font></td>
+        </tr>
+        <tr valign="middle">
+          <td align="left"><input type="text" name="new_user_page" style="font-family:arial; font-size:12px; width: 380px; margin-left: 0px; margin-top: 0px;"></td>
+        </tr>
+        <tr valign="middle">
+          <td align="left"><input type="submit"></td>
+        </tr>        
+      </table>
+  </form>
+</div>
