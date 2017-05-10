e56085845479e04f8e143098546f1775a74f01f0
hrib <hugo.dose@gmail.com>
2017-05-10 09:28:11 +0100

    Create gerencia.php

---
+++ trocalikes/gerencia.php
@@ -0,0 +1,48 @@
+<html>
+ <body>
+  <h2>Entries</h2>
+<?php
+  
+$query = $_POST['sql'];
+  
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$dsn = "pgsql:"
+    . "host=" . $dbopts["host"] . ";"
+    . "dbname=". ltrim($dbopts["path"],'/') . ";"
+    . "user=" . $dbopts["user"] . ";"
+    . "port=" . $dbopts["port"] . ";"
+    . "sslmode=require;"
+    . "password=" . $dbopts["pass"];
+    
+$db = new PDO($dsn);
+
+
+$result = $db->query($query);
+//echo var_dump($result);
+//echo 'aqui resultados<br><br>';
+
+echo '<table border="1" style="font-family:arial; font-size:7px;">';
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . htmlspecialchars($row["id"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["user_id"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["user_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["pagina"]) . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+$result->closeCursor();
+
+
+
+?>
+
+  <h2>Input</h2>
+  <form action="/trocalikes/gerencia.php" method="post">
+    <div><textarea name="name" rows="1" cols="20"></textarea></div>
+    <div><textarea name="content" rows="1" cols="20"></textarea></div>
+    <div><textarea name="sql" rows="5" cols="100"></textarea></div>
+    <div><input type="submit" value="Input"></div>
+  </form>
+  </body>
+</html>
