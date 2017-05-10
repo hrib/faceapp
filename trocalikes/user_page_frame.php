8d8baf349283eeb049053271fbd082b41bb155ac
hrib <hugo.dose@gmail.com>
2017-05-10 11:55:15 +0100

    Update user_page_frame.php

--- trocalikes/user_page_frame.php
+++ trocalikes/user_page_frame.php
@@ -22,24 +22,8 @@ $result = $db->query($query);
 
 echo $query;
 $_POST['new_user_page'] = NULL;
-echo "<script>window.top.location.href='user_page_frame.php'</script>";   
+//echo "<script>window.top.location.href='user_page_frame.php'</script>";   
 }
     
 ?>
 
-
-<div class="dentro">
-  <form action="user_page_frame.php" method="post">
-      <table  border="0">
-        <tr valign="middle">
-          <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:16px;"><b>Digite a URL da sua página nesse iframe: </b></font></td>
-        </tr>
-        <tr valign="middle">
-          <td align="left"><input type="text" name="new_user_page" style="font-family:arial; font-size:12px; width: 380px; margin-left: 0px; margin-top: 0px;"></td>
-        </tr>
-        <tr valign="middle">
-          <td align="left"><input type="submit"></td>
-        </tr>        
-      </table>
-  </form>
-</div>
