01c89826323d5f3d5bd44a98e7eec5e836c04c24
hrib <hugo.dose@gmail.com>
2017-05-10 10:36:38 +0100

    Update user_page_frame.php

--- trocalikes/user_page_frame.php
+++ trocalikes/user_page_frame.php
@@ -1,8 +1,36 @@
+<?php
+session_start(); 
+$pagina = $_POST['new_user_page'];
+if (isset($pagina )) { 
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$dsn = "pgsql:"
+    . "host=" . $dbopts["host"] . ";"
+    . "dbname=". ltrim($dbopts["path"],'/') . ";"
+    . "user=" . $dbopts["user"] . ";"
+    . "port=" . $dbopts["port"] . ";"
+    . "sslmode=require;"
+    . "password=" . $dbopts["pass"];
+    
+$db = new PDO($dsn);
+$user_name = $_SESSION["user_name"];
+$user_id = $_SESSION["user_id"];
+
+echo $user_id . " : " . $pagina ;
+$query = "UPDATE tl_cadastro SET pagina = '" . $pagina  . "' WHERE user_id = '" . $user_id . "';";
+$result = $db->query($query);
+echo $query;
+echo "<script>window.top.location.href='https://apps.facebook.com/trocalikes'</script>";   
+}
+    
+?>
+
+
 <div class="dentro">
-  <form action="save_user_page.php" method="post">
+  <form action="user_page_frame.php" method="post">
       <table  border="0">
         <tr valign="middle">
-          <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:16px;"><b>Digite a URL da sua página: </b></font></td>
+          <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:16px;"><b>Digite a URL da sua página nesse iframe: </b></font></td>
         </tr>
         <tr valign="middle">
           <td align="left"><input type="text" name="new_user_page" style="font-family:arial; font-size:12px; width: 380px; margin-left: 0px; margin-top: 0px;"></td>
