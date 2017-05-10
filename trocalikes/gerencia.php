1a518788c62aff2606c1b15ab4d1d153e1571369
hrib <hugo.dose@gmail.com>
2017-05-10 09:35:19 +0100

    Update gerencia.php

--- trocalikes/gerencia.php
+++ trocalikes/gerencia.php
@@ -27,12 +27,16 @@ if (isset($query)) {
   $result = $db->query($query);
 
   echo '<table border="1" style="font-family:arial; font-size:7px;">';
-  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+  while ($row = $result->fetch(PDO::FETCH_BOTH)) {
       echo "<tr>";
       echo "<td>" . htmlspecialchars($row["id"]) . "</td>";
       echo "<td>" . htmlspecialchars($row["user_id"]) . "</td>";
       echo "<td>" . htmlspecialchars($row["user_name"]) . "</td>";
       echo "<td>" . htmlspecialchars($row["pagina"]) . "</td>";
+      echo "<td>" . htmlspecialchars($row[0]) . "</td>";
+      echo "<td>" . htmlspecialchars($row[1]) . "</td>";
+      echo "<td>" . htmlspecialchars($row[2]) . "</td>";
+      echo "<td>" . htmlspecialchars($row[3]) . "</td>";
       echo "</tr>";
   }
   echo "</table>";
