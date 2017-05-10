7e52d9c3962742acc2b2ab88d6f18e629f7867c9
hrib <hugo.dose@gmail.com>
2017-05-10 10:42:15 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -81,30 +81,6 @@ echo $user_page . '<br>';
 
 echo "<iframe src='user_page_frame.php'></iframe>";
 
-?>
-<div class="dentro">
-  <form action="save_user_page.php" method="post">
-      <table  border="0">
-        <tr valign="middle">
-          <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:16px;"><b>Digite a URL da sua página: </b></font></td>
-        </tr>
-        <tr valign="middle">
-          <td align="left"><input type="text" name="new_user_page" style="font-family:arial; font-size:12px; width: 380px; margin-left: 0px; margin-top: 0px;"></td>
-        </tr>
-        <tr valign="middle">
-          <td align="left"><input type="submit"></td>
-        </tr>        
-      </table>
-  </form>
-</div>
-
-
-<?php
-
-
-
-
-
 
 
 try {  
