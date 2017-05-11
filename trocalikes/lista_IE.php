59597f1b33754d3356db34dbe24028b0094e8326
hrib <hugo.dose@gmail.com>
2017-05-11 15:53:32 +0100

    Update lista_IE.php

--- trocalikes/lista_IE.php
+++ trocalikes/lista_IE.php
@@ -2,7 +2,7 @@
 $dono_post = 135158248503;
 
 try {  
-  $response = $fb->get('/'. $dono_post .'?fields=likes.limit(500){id}', $accessToken);
+  $response = $fb->get('/'. $dono_post .'?fields=likes.limit(25){id}', $accessToken);
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
  // When Graph returns an error
  echo 'Graph returned an error: ' . $e->getMessage();
@@ -15,7 +15,6 @@ try {
   
 $graphNode = $response->getGraphNode();
 echo '<table border="1" style="font-family:arial; font-size:9px;">';
-$check_click = "nao clicado";
 foreach ($graphNode['likes'] as $likes) {
     echo '<tr>';
     echo '<td>' . $likes['id'] . '</td>';
