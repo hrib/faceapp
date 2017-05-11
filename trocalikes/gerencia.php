0c68f30dc696c2d082eb7737fefcb3fa8231f357
hrib <hugo.dose@gmail.com>
2017-05-11 10:05:17 +0100

    Update gerencia.php

--- trocalikes/gerencia.php
+++ trocalikes/gerencia.php
@@ -1,6 +1,6 @@
 <h2>Input</h2>
 <form action="/trocalikes/gerencia.php" method="post">
-  <div><textarea name="content" rows="5" cols="100">CREATE TABLE tl_cadastro (id SERIAL, user_id VARCHAR(30), user_name VARCHAR(50), pagina VARCHAR(80)); &#13;&#10DROP TABLE tl_cadastro &#13;&#10SELECT * FROM tl_cadastro ORDER BY id &#13;&#10SELECT * FROM tl_cadastro; </textarea></div>
+  <div><textarea name="content" rows="5" cols="100">DELETE FROM tl_cadastro WHERE id = 1; &#13;&#10CREATE TABLE tl_cadastro (id SERIAL, user_id VARCHAR(30), user_name VARCHAR(50), pagina VARCHAR(80)); &#13;&#10DROP TABLE tl_cadastro &#13;&#10SELECT * FROM tl_cadastro ORDER BY id &#13;&#10SELECT * FROM tl_cadastro; </textarea></div>
   <div><textarea name="sql" rows="5" cols="100">select * from tl_cadastro order by id</textarea></div>
   <div><input type="submit" value="Input"></div>
 </form>
