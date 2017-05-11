e184520fb38da54454e7ad61ab723594a54e083b
hrib <hugo.dose@gmail.com>
2017-05-11 11:46:34 +0100

    Update db_usuario.php

--- trocalikes/db_usuario.php
+++ trocalikes/db_usuario.php
@@ -16,7 +16,7 @@ $dsn = "pgsql:"
 $db = new PDO($dsn);
 
     
-    $query = "INSERT INTO tl_cadastro(user_id, user_name) SELECT '" . $user_id . "', '" . $user_name . "' where not exists (select id from tl_cadastro where user_id = '" . $user_id . "') RETURNING id;";
+    $query = "INSERT INTO tl_cadastro(tempo, user_id, user_name) SELECT now(), '" . $user_id . "', '" . $user_name . "' where not exists (select id from tl_cadastro where user_id = '" . $user_id . "') RETURNING id;";
     $result = $db->query($query);
     
     $query = "SELECT pagina FROM tl_cadastro WHERE user_id = '" . $user_id . "';";
