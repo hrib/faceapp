139d1ec994a2143d9d2335b42e192ea6088deb98
hrib <hugo.dose@gmail.com>
2017-05-11 13:40:50 +0100

    Update sql_queries.php

--- trocalikes/sql_queries.php
+++ trocalikes/sql_queries.php
@@ -23,6 +23,7 @@ function db_usuario($user_id, $user_name){
     
     $query = "INSERT INTO tl_cadastro(tempo, user_id, user_name) SELECT now(), '" . $user_id . "', '" . $user_name . "' where not exists (select id from tl_cadastro where user_id = '" . $user_id . "') RETURNING id;";
     $result = sql_query($query);
+    $result->closeCursor();
     
     $query = "SELECT pagina FROM tl_cadastro WHERE user_id = '" . $user_id . "';";
     $result = sql_query($query);
