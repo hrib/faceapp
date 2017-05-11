4f999911ed78a10236a7344662e40f201f82e791
hrib <hugo.dose@gmail.com>
2017-05-11 12:05:36 +0100

    Update gerencia.php

--- trocalikes/gerencia.php
+++ trocalikes/gerencia.php
@@ -11,8 +11,8 @@ DELETE FROM tl_cadastro WHERE id = 1;
 CREATE TABLE tl_cadastro (id SERIAL, tempo TIMESTAMP, user_id VARCHAR(30), user_name VARCHAR(50), pagina VARCHAR(80)); 
 DROP TABLE tl_cadastro; 
 SELECT * FROM tl_cadastro ORDER BY id; 
-CREATE TABLE tl_cliques (id SERIAL, tempo TIMESTAMP, dono_id VARCHAR(30), dono_page VARCHAR(80), dono_post VARCHAR(100), clicker_id VARCHAR(30), clicker_check INT);
-INSERT INTO tl_cliques (tempo , dono_id , dono_page , dono_post , clicker_id , clicker_check) VALUES (now(), 'dono id 123', 'dono page site', 'dono post postagem', 'click erid identidade', 0);  
+CREATE TABLE tl_cliques (id SERIAL, tempo TIMESTAMP, dono_id VARCHAR(30), dono_page VARCHAR(80), dono_post VARCHAR(100), clicker_id VARCHAR(30), clicker_check VARCHAR(15));
+INSERT INTO tl_cliques (tempo , dono_id , dono_page , dono_post , clicker_id , clicker_check) VALUES (now(), 'dono id 123', 'dono page site', 'dono post postagem', 'click erid identidade', 'esperando');  
     </textarea></div>
   <div><textarea name="sql" rows="5" cols="200"><?php echo $query; ?></textarea></div>
   <div><input type="submit" value="Input"></div>
