710c4947452760eb2e2f880fc1bd6111024ae362
hrib <hugo.dose@gmail.com>
2017-05-11 13:38:11 +0100

    Update sql_queries.php

--- trocalikes/sql_queries.php
+++ trocalikes/sql_queries.php
@@ -1,9 +1,4 @@
 <?php
-function todos_esperando(){
-
-    
-}
-
 
 function sql_query($query){
     
@@ -25,23 +20,12 @@ return $result;
 
 function db_usuario($user_id, $user_name){
     
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$dsn = "pgsql:"
-    . "host=" . $dbopts["host"] . ";"
-    . "dbname=". ltrim($dbopts["path"],'/') . ";"
-    . "user=" . $dbopts["user"] . ";"
-    . "port=" . $dbopts["port"] . ";"
-    . "sslmode=require;"
-    . "password=" . $dbopts["pass"];
-    
-$db = new PDO($dsn);
-
     
     $query = "INSERT INTO tl_cadastro(tempo, user_id, user_name) SELECT now(), '" . $user_id . "', '" . $user_name . "' where not exists (select id from tl_cadastro where user_id = '" . $user_id . "') RETURNING id;";
-    $result = $db->query($query);
+    $result = sql_query($query);
     
     $query = "SELECT pagina FROM tl_cadastro WHERE user_id = '" . $user_id . "';";
-    $result = $db->query($query);
+    $result = sql_query($query);
     $retorno = $result->fetch();
     return $retorno["pagina"];
     
