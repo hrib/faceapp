e0addacb1bd877ab3a36d693142f6fe593d370d8
hrib <hugo.dose@gmail.com>
2017-05-11 14:18:59 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -140,7 +140,7 @@ $retorno = sql_query("SELECT * FROM tl_cliques WHERE clicker_check = 'esperando'
       foreach($row as $item) {
         echo "<td>" . htmlspecialchars($item) . "</td>";
       }
-      $check_face = checa_clique_post($row['dono_post'], $row['clicker_id']);
+      $check_face = checa_clique_post($row['dono_post'], $row['clicker_id'], $accessToken);
       echo "<td>" . htmlspecialchars($row['dono_post']) . "</td>";
       echo "<td>" . htmlspecialchars($row['clicker_id']) . "</td>";
       echo "<td>" . htmlspecialchars($check_face) . "</td>";	
