4341fdf66efa2e138dfbf64e9e1ae64ea0d05d6b
hrib <hugo.dose@gmail.com>
2017-05-11 14:14:08 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -131,31 +131,6 @@ $(document).ready(function(){
 <?php
 
 
-try {  
-  $response = $fb->get('/'. $paginaID .'?fields=posts{likes{id,name}}', $accessToken);
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
- // When Graph returns an error
- echo 'Graph returned an error: ' . $e->getMessage();
- exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
- // When validation fails or other local issues
- echo 'Facebook SDK returned an error: ' . $e->getMessage();
- exit;
-}
-
-  
-$graphNode = $response->getGraphNode();
-echo '<table border="1" style="font-family:arial; font-size:9px;">';
-foreach ($graphNode['posts'] as $posts) {
-  foreach ($posts['likes'] as $likes) {
-    echo '<tr>';
-    echo '<td>' . $posts['id'] . '</td>';
-    echo '<td>' . $likes['name'] . '</td>';
-    echo '<td>' . $likes['id'] . '</td>';
-    echo '</tr>';
-  }
-} 
-echo '</table>';
 require_once('lista_rodrigo.php');
 
 $retorno = sql_query("SELECT * FROM tl_cliques WHERE clicker_check = 'esperando' ORDER BY id;"); 
