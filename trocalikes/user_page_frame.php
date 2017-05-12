94bb2e0d415a8b5f2d2b96c4b60a5c26c4d7790a
hrib <hugo.dose@gmail.com>
2017-05-12 23:44:37 +0100

    Update user_page_frame.php

--- trocalikes/user_page_frame.php
+++ trocalikes/user_page_frame.php
@@ -29,7 +29,9 @@ $user_id = $_SESSION["user_id"];
     
         $accessToken = $_SESSION["token"];
         
-        $paginaID = substr($pagina, 25, strlen($pagina) - 26);
+        //$paginaID = substr($pagina, 25, strlen($pagina) - 26);
+        $paginaID = $pagina
+        $paginaFull = 'https://www.facebook.com/'. $paginaID .'/';
         $erro = '';
         try {  
           $response = $fb->get('/'. $paginaID .'/?fields=posts.limit(50){id}', $accessToken);
@@ -52,7 +54,7 @@ $user_id = $_SESSION["user_id"];
             $erro = 'n'.$n_posts;
             if(isset($n_posts))
             {
-                $query = "UPDATE tl_cadastro SET pagina = '" . $pagina  . "' WHERE user_id = '" . $user_id . "';";
+                $query = "UPDATE tl_cadastro SET pagina = '" . $paginaFull  . "' WHERE user_id = '" . $user_id . "';";
                 $result = $db->query($query);
             }
         }
