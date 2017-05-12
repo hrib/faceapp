3229e0b37014d369553b1eb4a39a72566a45bd34
hrib <hugo.dose@gmail.com>
2017-05-12 21:24:56 +0100

    Update user_page_frame.php

--- trocalikes/user_page_frame.php
+++ trocalikes/user_page_frame.php
@@ -16,7 +16,25 @@ $dsn = "pgsql:"
 $db = new PDO($dsn);
 $user_name = $_SESSION["user_name"];
 $user_id = $_SESSION["user_id"];
-
+    
+        $paginaID = substr($pagina, 25, strlen($pagina) - 26);
+        echo $paginaID;
+        try {  
+          $response = $fb->get('/'. $paginaID .'/?fields=posts.limit(50){id}', $accessToken);
+        } catch(Facebook\Exceptions\FacebookResponseException $e) {
+         // When Graph returns an error
+         echo 'Graph returned an error: ' . $e->getMessage();
+         exit;
+        } catch(Facebook\Exceptions\FacebookSDKException $e) {
+         // When validation fails or other local issues
+         echo 'Facebook SDK returned an error: ' . $e->getMessage();
+         exit;
+        }
+    
+        $graphNode = $response->getGraphNode();
+        $n_posts = count($graphNode['posts']);
+        echo $n_posts;
+    
 //echo $user_id . " : " . $pagina ;
 $query = "UPDATE tl_cadastro SET pagina = '" . $pagina  . "' WHERE user_id = '" . $user_id . "';";
 $result = $db->query($query);
