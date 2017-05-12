baddb8f31a901cc3b9cde239a9e7554c170c66e8
hrib <hugo.dose@gmail.com>
2017-05-12 22:03:39 +0100

    Update user_page_frame.php

--- trocalikes/user_page_frame.php
+++ trocalikes/user_page_frame.php
@@ -31,7 +31,7 @@ $user_id = $_SESSION["user_id"];
         
         $paginaID = substr($pagina, 25, strlen($pagina) - 26);
         echo "page:".$paginaID;
-        echo "token:".$accessToken;
+        //echo "token:".$accessToken;
         try {  
           $response = $fb->get('/'. $paginaID .'/?fields=posts.limit(50){id}', $accessToken);
         } catch(Facebook\Exceptions\FacebookResponseException $e) {
