8e580d4348bb9327a52a6701afa882f970f52a4a
hrib <hugo.dose@gmail.com>
2017-05-12 19:55:12 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -138,8 +138,9 @@ $(document).ready(function(){
 //require_once('lista_rodrigo.php');
 //require_once('lista_IE.php');
 
+//VERIFICACAO
+//Verifica se cada "esperando" foi ou nao clicado, mudando para "clicado" 
 $retorno = sql_query("SELECT * FROM tl_cliques WHERE clicker_check = 'esperando' ORDER BY id;"); 
-
   echo '<table border="1" style="font-family:arial; font-size:7px;">';
   while ($row = $retorno->fetch(PDO::FETCH_ASSOC)) {
       echo "<tr>";
@@ -155,8 +156,13 @@ $retorno = sql_query("SELECT * FROM tl_cliques WHERE clicker_check = 'esperando'
   echo "</table>";
 $retorno->closeCursor();
 
+
+//LIMPEZA
+//Cancela todos os cliques "esperando" do cliente
 sql_query("UPDATE tl_cliques SET clicker_check = 'cancelado' WHERE clicker_id = '" . $_SESSION['user_id'] . "' AND clicker_check = 'esperando';"); 
 
+//CALCULO
+//recalcula todos os creditos e cria novos "gerados"
 $sobra = sql_query("SELECT coalesce(T1.clicker_id,  T2.dono_id) as usuario, (COALESCE(T1.n_creditos,0)) as Creditos, (COALESCE(T2.n_usados_prontos,0)) as Alocados, (COALESCE(T1.n_creditos,0) + COALESCE(T2.n_usados_prontos, 0)) as Sobra FROM (SELECT clicker_id, COUNT(*) as n_creditos FROM tl_cliques WHERE clicker_check = 'clicado' GROUP BY clicker_id) AS T1 FULL OUTER JOIN (SELECT dono_id, -COUNT(*) as n_usados_prontos FROM tl_cliques  WHERE clicker_check = 'gerado' OR clicker_check = 'esperando' OR clicker_check = 'clicado' GROUP BY dono_id) AS T2 ON T1.clicker_id = T2.dono_id;"); 
   echo '<table border="1" style="font-family:arial; font-size:7px;">';
   while ($row = $sobra->fetch(PDO::FETCH_ASSOC)) {
@@ -170,8 +176,14 @@ $sobra = sql_query("SELECT coalesce(T1.clicker_id,  T2.dono_id) as usuario, (COA
   echo "</table>";
 $sobra->closeCursor();
 
+//ALOCACAO
+//Seleciona os IDs das linhas "GERADO" e nao "ALOCADAS", mas que cliente NAO e' DONO e NAO clicou em post igual.
+//Tb NAO seleciona linhas com o mesmo POST
+$ids_selecionados = "SELECT id FROM (SELECT min(id) as id, min(tempo) as tempo, post_gerado FROM (SELECT id, post_gerado, tempo FROM (SELECT id, dono_post as post_gerado, tempo FROM tl_cliques WHERE  clicker_id IS NULL  AND clicker_check = 'gerado' AND dono_id <> '" . $_SESSION["user_id"] . "') AS T1 FULL OUTER JOIN  (SELECT dono_post as post_clicado FROM tl_cliques WHERE  clicker_id = '" . $_SESSION["user_id"] . "' AND clicker_check = 'clicado' GROUP BY dono_post) AS T2 ON post_gerado = post_clicado WHERE post_clicado IS NULL) AS TA GROUP BY post_gerado ORDER BY tempo LIMIT 9) FINAL";
+sql_query("UPDATE tl_cliques SET clicker_id = '" . $_SESSION["user_id"] . "', clicker_check = 'esperando' FROM (" . $ids_selecionados . ") AS T WHERE tl_cliques.id = T.id;");
 
-sql_query("UPDATE tl_cliques SET clicker_id = '" . $_SESSION["user_id"] . "', clicker_check = 'esperando' FROM (SELECT T1.id FROM (SELECT id, dono_post, tempo FROM tl_cliques WHERE  clicker_id IS NULL  AND clicker_check = 'gerado' AND dono_id <> '" . $_SESSION["user_id"] . "') AS T1 FULL OUTER JOIN  (SELECT dono_post FROM tl_cliques WHERE  clicker_id = '" . $_SESSION["user_id"] . "' AND clicker_check = 'clicado' GROUP BY dono_post) AS T2 ON T1.dono_post = T2.dono_post WHERE T2.dono_post IS NULL ORDER BY T1.tempo LIMIT 9) AS T WHERE tl_cliques.id = T.id;");
+//PROPAGANDA
+//Cria frames de acordo com oq foi alocado "ESPERANDO"
 $frames = sql_query("SELECT dono_post FROM tl_cliques WHERE clicker_id = '" . $_SESSION["user_id"] . "' AND clicker_check = 'esperando';");
   echo '<table border="0" style="font-family:arial; font-size:7px;">';
   while ($row = $frames->fetch(PDO::FETCH_ASSOC)) {
