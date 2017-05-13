9b426652c338d83e37bc76c2e8fca51630ba100b
hrib <hugo.dose@gmail.com>
2017-05-13 13:22:37 +0100

    Update my_queries.php

--- trocalikes/my_queries.php
+++ trocalikes/my_queries.php
@@ -38,12 +38,12 @@ try {
   $response = $fb->get('/'. $dono_post .'?fields=likes.limit(1000){id}', $accessToken);
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
  // When Graph returns an error
- echo 'Graph returned an error: ' . $e->getMessage();
- exit;
+ console.log($dono_post . ': Graph returned an error: ' . $e->getMessage());
+ //exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
  // When validation fails or other local issues
- echo 'Facebook SDK returned an error: ' . $e->getMessage();
- exit;
+ console.log($dono_post . ': Facebook SDK returned an error: ' . $e->getMessage());
+ //exit;
 }
   
 $graphNode = $response->getGraphNode();
