a90d2ccd798ab27afcd015bbec2bac29f3ffdc03
hrib <hugo.dose@gmail.com>
2017-05-13 01:25:14 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -141,15 +141,15 @@ $(document).ready(function(){
 <div align="left" id="user_page_frame">
       <table  border="0">
         <tr valign="middle">
-          <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:11px;"><b><span id="nome"><?php echo $_SESSION["user_name"]; ?></span></b></font></td>
-	  <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:11px;"><b><span id="texto_pagina">URL</span></b></font></td>
+          <td align="left"><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:11px;"><b><span id="nome"><?php echo $_SESSION["user_name"]; ?></span></b></font></td>
+	  <td><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:11px;"><span id="texto_pagina">URL</span></font></td>
           <td align="left"><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:11px;">https://www.facebook.com/<input type="text" id="form_user_page" value="<?php echo $user_page; ?>"  style="font-family:arial; font-size:12px; width: 380px; margin-left: 0px; margin-top: 0px;">/</font></td>
           <td align="left"><font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:11px;"><input type="submit" id="botao_pagina" value="Botao"></font></td>
         </tr>
 	</table>
 	<table  border="0"  style="background-color:white; display:none;" id="tabela_erro">
 	<tr valign="middle">	  
-	  <td><font style="font-family: Arial; color:red; font-size:12px;"><b><span id="resposta"></span></b></font></td>
+	  <td><font style="font-family: Arial; color:red; font-size:12px;"><span id="resposta"></span></font></td>
         </tr>
       </table>
 </div>
