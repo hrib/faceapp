b6ddd614d1918e3250ce06782742fc5e31521eb1
hrib <hugo.dose@gmail.com>
2017-05-13 20:52:46 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -47,9 +47,9 @@ if (! isset($accessToken)) {
   $permissions = ['public_profile']; // optionnal
   $loginUrl = $helper->getLoginUrl('https://apps.facebook.com/' . $app_name . '/', $permissions);
   //confirme que essa url de login esta autorizada no aplicativo
-  echo "<script>function reload(){window.top.location.href='".$loginUrl."';}></script>";
+  echo "<script>function logar(){window.top.location.href='".$loginUrl."';}></script>";
   echo "<div align='center' >";
-  echo "<input type='button' value='Login' onClick='reload()'>";
+  echo "<input type='button' value='Login' onClick='logar();'>";
   echo "</div>";
   
   exit;
