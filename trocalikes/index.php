bba3f916fad61f1ef425f94b08e3a4b435d366c5
hrib <hugo.dose@gmail.com>
2017-05-13 22:01:18 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -66,7 +66,7 @@ if (! isset($accessToken)) {
   echo "<script>function logar(){window.top.location.href='".$loginUrl."';}</script>";
   echo "<div align='center' valign='middle' >";
   echo "<input type='image' src='facebook-login.png'  width='400' height='50' value='Login' onClick='logar();'>";
-  echo "<input type='button' style='background: url(facebook-login.png); width:400px; height:50px;'  value='Login' onClick='logar();'>";
+  echo "<input type='button' style='background: url(facebook-login.png); background-size: 400px 50px; width:400px; height:50px;'  value='Login' onClick='logar();'>";
   //echo "<input type='button' src='facebook-login.png'  value='Login' onClick='logar();'>";
   echo "</div>";
   echo '<br><br><br><br><br><br><br><br><br><br>';	
