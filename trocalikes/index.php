ebb44ebe1a90d4a5df6995add24f1600d215bd5d
hrib <hugo.dose@gmail.com>
2017-05-13 14:59:45 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -276,7 +276,7 @@ $frames = sql_query("SELECT dono_post FROM tl_cliques WHERE clicker_id = '" . $_
 	$post_esperando_id = substr(stristr($item,'_',false),1);
 	//echo "<td>" . htmlspecialchars($page_esperando_id) . "</td>";
 	//echo "<td>" . htmlspecialchars($post_esperando_id) . "</td>";
-	echo "<td><iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" . $page_esperando_id . "%2Fposts%2F" . $post_esperando_id . "&width=500&height=500' width='500' height='600' ></iframe></td>";
+	echo "<td><iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F" . $page_esperando_id . "%2Fposts%2F" . $post_esperando_id . "&width=250'  width='500' height='720' style='border:none' scrolling='yes' frameborder='0'></iframe></td>";
       }
       if($z == 1){echo "</tr>";}
       $z = $z + 1;
