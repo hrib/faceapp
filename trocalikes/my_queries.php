b3d3f89443cd639439945f2b5325aefb20c5ff3a
hrib <hugo.dose@gmail.com>
2017-05-13 13:12:05 +0100

    Update my_queries.php

--- trocalikes/my_queries.php
+++ trocalikes/my_queries.php
@@ -91,12 +91,14 @@ function gerador_de_posts($fb, $accessToken, $usuario, $gera_n){
           $response = $fb->get('/'. $paginaID .'/?fields=posts.limit(50){id}', $accessToken);
         } catch(Facebook\Exceptions\FacebookResponseException $e) {
          // When Graph returns an error
-         echo 'Graph returned an error: ' . $e->getMessage();
-         exit;
+         echo 'Gerador de Posts > Graph returned an error: ' . $e->getMessage();
+         return;
+         //exit;
         } catch(Facebook\Exceptions\FacebookSDKException $e) {
          // When validation fails or other local issues
-         echo 'Facebook SDK returned an error: ' . $e->getMessage();
-         exit;
+         echo 'Gerador de Posts > Facebook SDK returned an error: ' . $e->getMessage();
+         return;
+         //exit;
         }
 
         $graphNode = $response->getGraphNode();
