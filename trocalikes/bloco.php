8bab064171f91e33ffe6e20f18e6f5e6b5122dee
hrib <hugo.dose@gmail.com>
2017-05-15 22:54:54 +0100

    Update bloco.php

--- trocalikes/bloco.php
+++ trocalikes/bloco.php
@@ -61,13 +61,6 @@ $sobra = sql_query("SELECT coalesce(T1.clicker_id,  T2.dono_id) as usuario, (COA
 $sobra->closeCursor();
 
 
-$creditos_cliente = sql_query("SELECT creditos, usados, saldo FROM (SELECT coalesce(T1.clicker_id,  T2.dono_id) as usuario, (COALESCE(T1.n_creditos,0)) as Creditos, (COALESCE(T2.n_usados,0)) as Usados, (COALESCE(T1.n_creditos,0) + COALESCE(T2.n_usados, 0)) as Saldo FROM (SELECT clicker_id, COUNT(*) as n_creditos FROM tl_cliques WHERE clicker_check = 'clicado' GROUP BY clicker_id) AS T1 FULL OUTER JOIN (SELECT dono_id, -COUNT(*) as n_usados FROM tl_cliques  WHERE clicker_check = 'clicado' GROUP BY dono_id) AS T2 ON T1.clicker_id = T2.dono_id) FINAL WHERE usuario = '" . $_SESSION["user_id"] ."';");
-while ($row = $creditos_cliente->fetch(PDO::FETCH_ASSOC)) {
-    $creditos = $row['creditos'];
-    $usados = -$row['usados'];
-    $saldo = $row['saldo'];
-}
-$creditos_cliente->closeCursor();
   
 
 
