286686e86a8a480b65e0598003e299b981fd6ca3
hrib <hugo.dose@gmail.com>
2017-05-15 21:44:14 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -352,11 +352,12 @@ iframe3
 <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D1072132882921227%26id%3D798157940318724&width=500" width="500" height="305" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 <br>
 fim iframe
+<input type="button" value="Mais Posts >>" onClick="window.location=window.location;">
 -->
 <font style="font-family: Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:11px;">
 <br>
 <div align="center" >
-<input type="button" value="Mais Posts >>" onClick="window.location=window.location;">
+<a href="index.php">Mais Posts >></a>
 </div>
 <br>
 <br>
