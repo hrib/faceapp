ef41c8fcf234096130424181825c1b9a06ecb518
hrib <hugo.dose@gmail.com>
2017-05-15 18:48:21 +0100

    Update index.php

--- trocalikes/index.php
+++ trocalikes/index.php
@@ -42,8 +42,8 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => 'v2.9',
   ]);
 
-//$helper = $fb->getCanvasHelper();
-$helper = $fb->getRedirectLoginHelper();
+$helper = $fb->getCanvasHelper();
+//$helper = $fb->getRedirectLoginHelper();
 try {
   $accessToken = $helper->getAccessToken();
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
@@ -62,9 +62,9 @@ if (! isset($accessToken)) {
   //echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
   $helper = $fb->getRedirectLoginHelper();
   $permissions = ['public_profile']; // optionnal
-  //$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/' . $app_name . '/', $permissions);
+  $loginUrl = $helper->getLoginUrl('https://apps.facebook.com/' . $app_name . '/', $permissions);
   //$loginUrl = $helper->getLoginUrl('https://m.facebook.com/apps/' . $app_name . '/', $permissions);	
-  $loginUrl = $helper->getLoginUrl('https://apostagol.herokuapp.com/' . $app_name . '/', $permissions);	
+  //$loginUrl = $helper->getLoginUrl('https://apostagol.herokuapp.com/' . $app_name . '/', $permissions);	
   //confirme que essa url de login esta autorizada no aplicativo
   echo '<br><br><br><br><br><br><br>';
   echo "<script>function logar(){window.top.location.href='".$loginUrl."';}</script>";
