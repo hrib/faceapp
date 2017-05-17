8e21f7c451c19f7f8857fb32db0862c2eaa3b931
hrib <hugo.dose@gmail.com>
2017-05-17 14:01:30 +0100

    Update gerenciateste.php

--- trocalikes/gerenciateste.php
+++ trocalikes/gerenciateste.php
@@ -6,7 +6,7 @@ echo $query;
 ?>
 
 <h2>Input</h2>
-<form action="/trocalikes/gerencia.php" method="post">
+<form action="/trocalikes/gerenciateste.php" method="post">
   <div>
     <textarea name="content" rows="10" cols="200">
 DELETE FROM tl_cadastro WHERE id = 1; 
@@ -20,13 +20,13 @@ INSERT INTO tl_cliques (tempo , dono_id , dono_page , dono_post , clicker_id , c
   <div><input type="submit" value="Input"></div>
 </form>
 
-<form action="/trocalikes/gerencia.php" method="post">
+<form action="/trocalikes/gerenciateste.php" method="post">
   <div><textarea name="sql2" rows="5" cols="200">SELECT * FROM tl_cadastro ORDER BY id</textarea></div>
   <div><input type="submit" value="Lista Cadastro"></div>
 </form>
 
 
-<form action="/trocalikes/gerencia.php" method="post">
+<form action="/trocalikes/gerenciateste.php" method="post">
   <div><textarea name="sql2" rows="5" cols="200">SELECT * FROM ((SELECT coalesce(T1.clicker_id,  T2.dono_id) as usuario, (COALESCE(T1.n_creditos,0)) as Creditos, (COALESCE(T2.n_usados,0)) as Usados, (COALESCE(T1.n_creditos,0) + COALESCE(T2.n_usados, 0)) as Sobra FROM (SELECT clicker_id, COUNT(*) as n_creditos FROM tl_cliques WHERE clicker_check = 'clicado' GROUP BY clicker_id) AS T1 FULL OUTER JOIN (SELECT dono_id, -COUNT(*) as n_usados FROM tl_cliques  WHERE clicker_check = 'clicado' GROUP BY dono_id) AS T2 ON T1.clicker_id = T2.dono_id) AS TA FULL OUTER JOIN (SELECT user_id, user_name FROM tl_cadastro) AS TB ON TA.usuario = TB.user_id)</textarea></div>
   <div><input type="submit" value="Creditos"></div>
 </form>
