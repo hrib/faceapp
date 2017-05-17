1289956835a9f9864a791faab031ce86bd4f6a84
hrib <hugo.dose@gmail.com>
2017-05-17 16:00:06 +0100

    Update gerenciateste.php

--- trocalikes/gerenciateste.php
+++ trocalikes/gerenciateste.php
@@ -1,7 +1,7 @@
 
 
 <h2>Input</h2>
-<form action="/trocalikes/gerenciateste2.php" method="post">
+<form action="/trocalikes/gerenciateste2.php" method="post" enctype="multipart/form-data">
   <div>
     <textarea name="content" rows="10" cols="200">
 SELECT clicker_id, COUNT(*) as n_creditos FROM tl_cliques WHERE clicker_check = 'clicado' GROUP BY clicker_id
