b1c1d0840f8090d2d80349afbd77716d7f0d7942
hrib <hugo.dose@gmail.com>
2017-05-20 15:50:38 +0100

    Update index.php

--- posta/index.php
+++ posta/index.php
@@ -8,8 +8,8 @@ $userToken = getenv('FB_TESTE_HM_USERTOKEN');
 $postid = '1717137268528382_1934505183458255';
 
 $agora = date("Y-m-d");
-$chaves = array('divulgacao','blog+pessoal');
-$chaves = array('blog+pessoal');
+$chaves = array('divulgacao','blog pessoal');
+$chaves = array('blog pessoal');
 $chave = $chaves[mt_rand(0, sizeof($chaves) - 1)];
 
 $limite = mt_rand(1000, 5000);
