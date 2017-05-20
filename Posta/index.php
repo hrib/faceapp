d0cb0ff14d45c9f7503b557a31e6ae538ea6f8f1
hrib <hugo.dose@gmail.com>
2017-05-20 01:26:26 +0100

    Update index.php

--- Posta/index.php
+++ Posta/index.php
@@ -3,7 +3,11 @@ session_start();
 require_once(dirname(__FILE__)."/../src/Facebook/autoload.php");
 $app_id = getenv('FB_APP_ID');
 $app_secret = getenv('FB_APP_SECRET');
+$userToken = getenv('FB_TESTE_HM_USERTOKEN');
 
+$postid = '1092972697443376_1485285631545412';
+$texto = '!';
+    
 $fb = new Facebook\Facebook([
     'app_id' => $app_id,
     'app_secret' => $app_secret,
@@ -12,13 +16,13 @@ $fb = new Facebook\Facebook([
   ]);
 
 $data = [
-  'source' => $fb->fileToUpload($media),
   'message' => $texto,
 ];
-  
-  
+
+$target = '/' . $postid . '/comments';
+ 
 try {
-   $response = $fb->post($target, $data, $page_access_token);
+   $response = $fb->post($target, $data, $userToken);
  } catch(Facebook\Exceptions\FacebookResponseException $e) {
    // When Graph returns an error
    echo 'Graph returned an error: ' . $e->getMessage();
