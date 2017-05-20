236a369dd74aca21a9170254674f8520b40b0a07
hrib <hugo.dose@gmail.com>
2017-05-20 15:14:02 +0100

    Update index.php

--- posta/index.php
+++ posta/index.php
@@ -40,7 +40,7 @@ while($graphNode) {
             echo '<td>' . $pagina['posts'][0]['id'] . '</td>';
             echo '<td>' . date_format($pagina['posts'][0]['created_time'], 'Y-m-d') . '</td>';
             echo '<td>' . $pagina['posts'][0]['story'] . '</td>';
-            if((date_format($pagina['posts'][0]['created_time'], 'Y-m-d') == $agora) AND ($pagina['fan_count'] < 2000)){
+            if((date_format($pagina['posts'][0]['created_time'], 'Y-m-d') == $agora) AND ($pagina['fan_count'] < 200)){
                 echo '<td>X</td>';
                 pagina_post_comenta($fb, $pagina['posts'][0]['id'], $userToken);
             } else {
