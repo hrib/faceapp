3488bb27c72b6fc477c53fe5aaccc5fd3eba735b
hrib <hugo.dose@gmail.com>
2018-01-06 19:48:06 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -5,10 +5,9 @@ $dest = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/
 list($width, $height) = getimagesize('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
 imagecopyresampled($dest, $src, 0, 0, 0, 0, $width, $height, 1470, 1471);
 
-
 $media = 'media.jpg';
 imagejpeg($dest, $media);  
-
+echo '<img src="media.jpg">';
 
 imagedestroy($dest);
 imagedestroy($src);
