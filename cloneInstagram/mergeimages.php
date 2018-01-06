07879b55431ba4e772dd3a646fbcb2eb849b739e
hrib <hugo.dose@gmail.com>
2018-01-06 18:08:14 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,25 +1,24 @@
 <?php
-$dest = imagecreatefrompng('https://quique123.files.wordpress.com/2013/03/screen-shot-2013-03-02-at-11-40-03-am.png');
-$src = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
+$src = imagecreatefrompng('https://quique123.files.wordpress.com/2013/03/screen-shot-2013-03-02-at-11-40-03-am.png');
+$dest = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
 //header('Content-Type: image/png');
 //imagepng($dest);
 //header('Content-Type: image/jpeg');
 //imagejpeg($src);
 
 
-imagealphablending($dest, false);
-imagesavealpha($dest, true);
+//imagealphablending($dest, false);
+//imagesavealpha($dest, true);
 
-//imagecopymerge($dest, $src, 0, 0, 0, 0, 550, 367, 100); //have to play with these numbers for it to work for you, etc.
-
-imagecopymerge($src, $dest, 0, 0, 0, 0, 550, 367, 100); //have to play with these numbers for it to work for you, etc.
+imagecopyresampled($dest, $src, $src2x, $src2y, 0, 0, $src2w, $src2h, $src2w, $src2h);
+//imagecopymerge($src, $dest, 0, 0, 0, 0, 550, 367, 100); //have to play with these numbers for it to work for you, etc.
 
 
 //header('Content-Type: image/png');
 //imagepng($dest);
 
 header('Content-Type: image/jpeg');
-imagejpeg($src);
+imagejpeg($dest);
 
 
 imagedestroy($dest);
