c938092101c191c8d393339b645b0987cacad2ee
hrib <hugo.dose@gmail.com>
2018-01-06 18:25:35 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,5 +1,5 @@
 <?php
-$src = imagecreatefrompng('https://seeklogo.com/images/C/Clube_de_Regatas_do_Flamengo-logo-4F9152B932-seeklogo.com.png');
+$src = imagecreatefrompng('http://78.media.tumblr.com/caf79905a6fc06b378bee73ad0d87368/tumblr_oi1eqkSvdL1vl7x77o1_1280.png');
 $dest = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
 //header('Content-Type: image/png');
 //imagepng($dest);
@@ -11,7 +11,7 @@ $dest = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/
 //imagesavealpha($dest, true);
 
 //imagecopyresampled($dest, $src, $src2x, $src2y, 0, 0, $src2w, $src2h, $src2w, $src2h);
-imagecopyresampled($dest, $src, 0, 0, 0, 0, 550, 367, 550, 367);
+imagecopyresampled($dest, $src, 0, 0, 0, 0, 550, 367, 1080, 1080);
 
 //imagecopymerge($src, $dest, 0, 0, 0, 0, 550, 367, 100); //have to play with these numbers for it to work for you, etc.
 
