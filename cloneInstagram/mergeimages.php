11fe81982f804d428083a51375d7fa5db16c9670
hrib <hugo.dose@gmail.com>
2018-01-06 18:17:50 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,5 +1,5 @@
 <?php
-$src = imagecreatefrompng('https://quique123.files.wordpress.com/2013/03/screen-shot-2013-03-02-at-11-40-03-am.png');
+$src = imagecreatefrompng('https://seeklogo.com/images/C/Clube_de_Regatas_do_Flamengo-logo-4F9152B932-seeklogo.com.png');
 $dest = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
 //header('Content-Type: image/png');
 //imagepng($dest);
