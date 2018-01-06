ef5f030977a4027df040d1416dbb554cc2bc33d3
hrib <hugo.dose@gmail.com>
2018-01-06 19:38:21 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,29 +1,13 @@
 <?php
 $src = imagecreatefrompng('https://github.com/hrib/faceapp/raw/master/cloneInstagram/IMG-2975.PNG');
 $dest = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
-//header('Content-Type: image/png');
-//imagepng($dest);
-//header('Content-Type: image/jpeg');
-//imagejpeg($src);
 
-
-//imagealphablending($dest, false);
-//imagesavealpha($dest, true);
-
-//imagecopyresampled($dest, $src, $src2x, $src2y, 0, 0, $src2w, $src2h, $src2w, $src2h);
 list($width, $height) = getimagesize('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
 imagecopyresampled($dest, $src, 0, 0, 0, 0, $width, $height, 1470, 1471);
-//imagecopyresampled($dest, $src, 0, 0, 0, 0, $dest2w, $dest2h, 1470, 1471);
-//imagecopyresampled($dest, $src, 0, 0, 0, 0, 1470, 1471, 1470, 1471);
-
-//imagecopymerge($src, $dest, 0, 0, 0, 0, 550, 367, 100); //have to play with these numbers for it to work for you, etc.
 
 
-//header('Content-Type: image/png');
-//imagepng($dest);
-
-header('Content-Type: image/jpeg');
-imagejpeg($dest);
+$media = 'media.jpg';
+imagejpeg($dest, $media);  
 
 
 imagedestroy($dest);
