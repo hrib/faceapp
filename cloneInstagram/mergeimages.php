3729f8b72abb66fb6a67c994bb32c9cc3783e355
hrib <hugo.dose@gmail.com>
2018-01-06 19:58:24 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,6 +1,6 @@
 <?php
 $src = imagecreatefrompng('https://github.com/hrib/faceapp/raw/master/cloneInstagram/IMG-2975.PNG');
-$dest = imagecreatefromjpeg('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
+$dest = imagecreatefromjpeg('http://www.aliciafashionista.com/wp-content/uploads/2016/12/IMG_5867.jpg');
 
 list($width, $height) = getimagesize('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
 $fator = $width/$height;
