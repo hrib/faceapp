8c6e7adc66b2036b55f1c8046bddef419beb956e
hrib <hugo.dose@gmail.com>
2018-01-07 00:20:50 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -5,7 +5,7 @@ $dest = imagecreatefromjpeg('http://www.aliciafashionista.com/wp-content/uploads
 list($width, $height) = getimagesize('https://media-cdn.tripadvisor.com/media/photo-s/0e/85/48/e6/seven-mile-beach-grand.jpg');
 $dimensao = min($width,$height);
 $diff_width = $width - $dimensao;
-$diff_$height = $height - $dimensao;
+$diff_height = $height - $dimensao;
 
 //imagecopyresampled ( resource $dst_image , resource $src_image , int $dst_x , int $dst_y , int $src_x , int $src_y , int $dst_w , int $dst_h , int $src_w , int $src_h )
 //imagecopyresampled($dest, $src, 0, 0, 0, 0, $width, $height, 1470, 1470);
