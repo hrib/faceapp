ed139a66eeec1a8fffb9cafedce74c948a1ff4e0
hrib <hugo.dose@gmail.com>
2018-01-07 01:02:34 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,14 +1,14 @@
 <?php
-$src = imagecreatefrompng('https://github.com/hrib/faceapp/raw/master/cloneInstagram/IMG-2975.PNG');
+$src = imagecreatefrompng('https://github.com/hrib/faceapp/raw/master/cloneInstagram/IMG-2976.PNG');
 $dest = imagecreatefromjpeg('https://www.thesun.co.uk/wp-content/uploads/2016/06/nintchdbpict000247118702.jpg?w=475');
 
 list($width, $height) = getimagesize('https://www.thesun.co.uk/wp-content/uploads/2016/06/nintchdbpict000247118702.jpg?w=475');
 $dimensao = min($width,$height);
 $diff_width = $width - $dimensao;
 $diff_height = $height - $dimensao;
-echo '' . $diff_width . ', ' .  $diff_height . ', ' .  0 . ', ' .  0 . ', ' .  $dimensao . ', ' .  $dimensao . ', ' .  1470 . ', ' .  1470;
+echo '' . $diff_width . ', ' .  $diff_height . ', ' .  0 . ', ' .  0 . ', ' .  $dimensao . ', ' .  $dimensao . ', ' .  1470 . ', ' .  1470 . '<br><br>';
 //imagecopyresampled ( resource $dst_image , resource $src_image , int $dst_x , int $dst_y , int $src_x , int $src_y , int $dst_w , int $dst_h , int $src_w , int $src_h )
-//imagecopyresampled($dest, $src, 0, 0, 0, 0, $width, $height, 1470, 1470);
+//imagecopyresampled($dest, $src, $diff_width, $diff_height, 0, 0, $dimensao, $dimensao, 1470, 1470);
 imagecopyresampled($dest, $src, $diff_width, $diff_height, 0, 0, $dimensao, $dimensao, 1470, 1470);
 
 
