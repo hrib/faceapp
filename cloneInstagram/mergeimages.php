25a9deae5f4764c90ed8d918bb3c7a211476ab0c
hrib <hugo.dose@gmail.com>
2018-01-07 00:37:20 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -6,7 +6,7 @@ list($width, $height) = getimagesize('https://media-cdn.tripadvisor.com/media/ph
 $dimensao = min($width,$height);
 $diff_width = $width - $dimensao;
 $diff_height = $height - $dimensao;
-
+echo '' . $diff_width . ', ' .  $diff_height . ', ' .  0 . ', ' .  0 . ', ' .  $dimensao . ', ' .  $dimensao . ', ' .  1470 . ', ' .  1470;
 //imagecopyresampled ( resource $dst_image , resource $src_image , int $dst_x , int $dst_y , int $src_x , int $src_y , int $dst_w , int $dst_h , int $src_w , int $src_h )
 //imagecopyresampled($dest, $src, 0, 0, 0, 0, $width, $height, 1470, 1470);
 imagecopyresampled($dest, $src, $diff_width, $diff_height, 0, 0, $dimensao, $dimensao, 1470, 1470);
