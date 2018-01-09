b831df4c4248e50ba482416529604b4df7f038a2
hrib <hugo.dose@gmail.com>
2018-01-09 16:52:51 +0000

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,8 +1,8 @@
 <?php
 $src = imagecreatefrompng('https://github.com/hrib/faceapp/raw/master/cloneInstagram/IMG_3029.PNG');
-$dest = imagecreatefromjpeg('https://github.com/hrib/faceapp/raw/master/cloneInstagram/IMG_3029.PNG');
+$dest = imagecreatefromjpeg('https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg');
 
-list($width, $height) = getimagesize('https://github.com/hrib/faceapp/raw/master/cloneInstagram/IMG_3029.PNG');
+list($width, $height) = getimagesize('https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg');
 $dimensao = min($width,$height);
 $diff_width = $width - $dimensao;
 $diff_height = $height - $dimensao;
