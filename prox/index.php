f96a33091e26040415b3374cec23a06c93320181
hrib <hugo.dose@gmail.com>
2018-02-28 09:53:26 +0000

    Update index.php

--- prox/index.php
+++ prox/index.php
@@ -1,48 +1,7 @@
 <?php
-define('CONFIG_FILE', 'simple-php-proxy_config.php');
-/*
-    Installation note:
-    Copy example.simple-php-proxy_config.php to simple-php-proxy_config.php
-    in the same folder as this script or to one or two levels above it.
-    Optionally you can change the variables right here and not use the config file.
-    For an explanation of the config variables see example.simple-php-proxy_config.php
-*/
-//$dest_host = "bash.org";
-//$proxy_base_url = '/';
-
-//$dest_host = "globoesporte.globo.com";
-//echo $dest_host . '<br>';
-$proxy_base_url = '/prox';
-
 $proxied_headers = array('Set-Cookie', 'Content-Type', 'Cookie', 'Location');
-// Variables you specify in the config file overwrite variables set above.
-foreach( array('./', '../', '../../') as $path_rel )
-{
-    if( file_exists(dirname(__file__)."/$path_rel" . CONFIG_FILE) )
-    { 
-        include(dirname(__file__)."/$path_rel" . CONFIG_FILE);
-        break;
-    }
-}
-//canonical trailing slash
-$proxy_base_url_canonical = rtrim($proxy_base_url, '/ ') . '/';
-//check if valid
-if( strpos($_SERVER['REQUEST_URI'], $proxy_base_url) !== 0 )
-{
-    die("The config paramter \$prox_base_url \"$proxy_base_url\" that you specified 
-        does not match the beginning of the request URI: ".
-        $_SERVER['REQUEST_URI']);
-}
-//remove base_url and optional index.php from request_uri
-$proxy_request_url = substr($_SERVER['REQUEST_URI'], strlen($proxy_base_url_canonical));
-echo $proxy_request_url . '<br>';
-if( strpos($proxy_request_url, 'index.php') === 0 )
-{
-    $proxy_request_url = ltrim(substr($proxy_request_url, strlen('index.php')), '/');
-}
-//final proxied request url
-//$proxy_request_url = "http://" . rtrim($dest_host, '/ ') . '/' . $proxy_request_url;
 
+$proxy_request_url = $_GET['link'];
 echo $proxy_request_url . '<br>';
 /* Init CURL */
 $ch = curl_init();
