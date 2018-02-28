62dc2afd45536684a7f02b7732824191a2dde397
hrib <hugo.dose@gmail.com>
2018-02-28 10:32:19 +0000

    Update index.php

--- prox/index.php
+++ prox/index.php
@@ -69,12 +69,12 @@ foreach($proxied_headers as $hname)
 }
 
 echo '<br><br>**************************************************************<br><br>';
-echo '<iframe srcdoc="' . print_r($body). '"></iframe>';
+echo '<iframe srcdoc="' . print_r($body). '" width="1000" height="800" scrolling="yes"></iframe>';
 //var_dump($body);
 
 //echo '<br><br>**************************************************************<br><br>';
 //var_dump($body);
-echo '<br><br>**************************************************************<br><br>';
-echo '<div>';
-die($body);
-echo '</div>';
+//echo '<br><br>**************************************************************<br><br>';
+//echo '<div>';
+//die($body);
+//echo '</div>';
