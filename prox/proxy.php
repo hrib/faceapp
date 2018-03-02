12a6d206ea3ea6943db2c49bdfa77df6533cf015
hrib <hugo.dose@gmail.com>
2018-03-02 14:24:18 +0000

    Update proxy.php

--- prox/proxy.php
+++ prox/proxy.php
@@ -1,13 +1,13 @@
 <?php
 echo $_GET['site'] . '<br>';
 
+$context = stream_context_create([
+    'http' => [
+        'user_agent' => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)',
+    ],
+]);
+$html = file_get_contents($_GET['site'], false, $context);
 
+print_r($http_response_header); // see response headers
 
-
-
-if(isset($_GET['site'])){
-  header('Content-Type: text/html');
-  $string = file_get_contents($_GET['site']);
-  echo $string;
-}
 ?>
