2f692e934ecf91f00a04fabc538cbb71e31ade1e
hrib <hugo.dose@gmail.com>
2018-03-02 14:05:13 +0000

    Update proxy.php

--- prox/proxy.php
+++ prox/proxy.php
@@ -1,6 +1,9 @@
 <?php
+echo $_GET['site'] . '<br>';
+
 if(isset($_GET['site'])){
   $f = fopen($_GET['site'], 'r');
+  echo var_dump($f) . '<br>';
   $html = '';
   while (!feof($f)) {
     $html .= fread($f, 24000);
