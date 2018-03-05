8309a107131ea1b31fb4301027bc09bc79d61de2
hrib <hugo.dose@gmail.com>
2018-03-05 16:57:43 +0000

    Update proxy.php

--- prox/proxy.php
+++ prox/proxy.php
@@ -4,24 +4,6 @@ echo $_GET['site'] . '<br>';
 
 $Url = $_GET['site'];
 
-
-$header=array('GET /1575051 HTTP/1.1',
-'Host: adfoc.us',
-'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Language:en-US,en;q=0.8',
-'Cache-Control:max-age=0',
-'Connection:keep-alive',
-'Host:adfoc.us',
-'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36',
-);
-
-
-
-if (!function_exists('curl_init')){
-    die('CURL is not installed!');
-}
-
-
 $options = array(
   'http'=>array(
     'method'=>"GET",
@@ -34,7 +16,6 @@ $options = array(
 
 $context = stream_context_create($options);
 
-// Open the file using the HTTP headers set above
 $file = file_get_contents($Url, false, $context);
 echo $file;
 
