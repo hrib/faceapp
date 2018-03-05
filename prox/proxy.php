6f5fbf218d095af334a6d74ee6714628851b299c
hrib <hugo.dose@gmail.com>
2018-03-05 22:50:25 +0000

    Update proxy.php

--- prox/proxy.php
+++ prox/proxy.php
@@ -22,3 +22,39 @@ echo htmlspecialchars($file);
 
 
 ?>
+
+<script type="text/javascript">
+
+var anterior = '';
+var url = "<?php echo $Url ?>";
+
+time=setInterval(function(){
+
+var x = showHint(url);
+function showHint(str) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               var n = this.responseText.search('player.twitch.tv');
+               var meio = this.responseText.substring(n-8);
+               var m = meio.search('&');
+               var fim = meio.substring(0,m);
+               console.log(anterior + ' x ' + fim);
+	       if(fim != anterior) {
+	               document.documentElement.innerHTML = n + ' ' + m + ' ' + fim +' <iframe src="'+fim+'" style="border: 0; width: 100%; height: 100%">Your browser doesnt support iFrames.</iframe>';
+                       anterior = fim;
+                       console.log('NOVA URL:' + fim);
+	       
+	       }
+            }
+        };
+        xmlhttp.open("GET", "proxy.php?site=" + str, true);
+        xmlhttp.send();
+}
+
+
+},10000);
+</script>
+
+
+
