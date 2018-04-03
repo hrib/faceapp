1a25e8375d30ee433d825c5204fe642f103decce
hrib <hugo.dose@gmail.com>
2018-04-03 10:05:12 +0100

    Update teste.php

--- prox/teste.php
+++ prox/teste.php
@@ -1,3 +1,16 @@
 <?php
-echo 'oi';
+echo $_GET['site'] . '<br>';
+$Url = $_GET['site'];
+$options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+              "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
+  )
+);
+$context = stream_context_create($options);
+$file = file_get_contents($Url, false, $context);
+//echo $file;
+echo htmlspecialchars($file);
 ?>
