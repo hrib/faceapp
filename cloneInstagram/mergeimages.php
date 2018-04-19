7ae55f08a772784c0d0e28092b2c1aaf364e1618
hrib <hugo.dose@gmail.com>
2018-04-19 09:01:54 +0100

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -1,5 +1,5 @@
 <?php
-$src = imagecreatefrompng('https://github.com/hrib/faceapp/raw/master/cloneInstagram/Adobe_20180412_110534.png');
+$src = imagecreatefrompng('https://wildtrails.in/wp-content/uploads/2015/06/Tiger-transparent.png');
 $dest = imagecreatefromjpeg('https://scontent-lht6-1.cdninstagram.com/vp/6f963c462b785c34af0325ea26e68095/5B56B7C0/t51.2885-15/e35/29717837_1713263295379805_3593801708695715840_n.jpg');
 
 //list($width, $height) = getimagesize('https://scontent-lht6-1.cdninstagram.com/vp/6f963c462b785c34af0325ea26e68095/5B56B7C0/t51.2885-15/e35/29717837_1713263295379805_3593801708695715840_n.jpg');
