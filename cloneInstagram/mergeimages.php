c27968e423cf5625d4b12ba6a74768514855b7df
hrib <hugo.dose@gmail.com>
2018-04-19 08:58:55 +0100

    Update mergeimages.php

--- cloneInstagram/mergeimages.php
+++ cloneInstagram/mergeimages.php
@@ -3,13 +3,17 @@ $src = imagecreatefrompng('https://github.com/hrib/faceapp/raw/master/cloneInsta
 $dest = imagecreatefromjpeg('https://scontent-lht6-1.cdninstagram.com/vp/6f963c462b785c34af0325ea26e68095/5B56B7C0/t51.2885-15/e35/29717837_1713263295379805_3593801708695715840_n.jpg');
 
 //list($width, $height) = getimagesize('https://scontent-lht6-1.cdninstagram.com/vp/6f963c462b785c34af0325ea26e68095/5B56B7C0/t51.2885-15/e35/29717837_1713263295379805_3593801708695715840_n.jpg');
+list($src_width, $src_height) = getimagesize($src);
+$dimensao_src = min($src_width,$src_height);
 list($width, $height) = getimagesize($dest);
 $dimensao = min($width,$height);
 $diff_width = $width - $dimensao;
 $diff_height = $height - $dimensao;
-echo '' . $diff_width . ', ' .  $diff_height . ', ' .  0 . ', ' .  0 . ', ' .  $dimensao . ', ' .  $dimensao . ', ' .  654 . ', ' .  654 . '<br><br>';
+echo '' . $width . ', ' .  $height . ', ' .  $src_width . ', ' .  $src_height .'<br><br>';
+
+echo '' . $diff_width . ', ' .  $diff_height . ', ' .  0 . ', ' .  0 . ', ' .  $dimensao . ', ' .  $dimensao . ', ' .  $dimensao_src . ', ' .  $dimensao_src . '<br><br>';
 //imagecopyresampled ( resource $dst_image , resource $src_image , int $dst_x , int $dst_y , int $src_x , int $src_y , int $dst_w , int $dst_h , int $src_w , int $src_h )
-imagecopyresampled($dest, $src, $diff_width, $diff_height, 0, 0, $dimensao, $dimensao, 905, 905);
+imagecopyresampled($dest, $src, $diff_width, $diff_height, 0, 0, $dimensao, $dimensao, $dimensao_src, $dimensao_src);
 
 
 $media = 'media.jpg';
